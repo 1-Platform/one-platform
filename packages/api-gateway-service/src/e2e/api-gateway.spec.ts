da79079d0288766f63d3ff80727bf5d572989720
Rigin Oommen <riginoommen@gmail.com>
2020-04-23 22:52:32 +0530

    Included more parameter for test cases

--- packages/api-gateway-service/src/e2e/api-gateway.spec.ts
+++ packages/api-gateway-service/src/e2e/api-gateway.spec.ts
@@ -4,11 +4,16 @@ import ApiGateway from '../../service';
 
 /* Supertest */
 import supertest from 'supertest';
+// import * as async from 'async';
 
 let request: supertest.SuperTest<supertest.Test>;
 const query = `
   fragment userType on UserType {
       _id
+      name
+      uid
+      name
+      title
   }
 
   query ListUsers {
@@ -18,8 +23,13 @@ const query = `
   }
 `;
 
-beforeAll( () => {
-  request = supertest.agent( ApiGateway );
+beforeAll( async () => {
+  const promise = await new Promise( ( resolve, reject ) => {
+    request = supertest.agent( ApiGateway );
+    setTimeout( resolve, 100 );
+  } );
+
+  Promise.all( [ promise ] ).then( ( values ) => values );
 } );
 afterAll( done => {
   return ApiGateway.close( done );
@@ -35,6 +45,12 @@ describe( 'API Gateway Test for Testing queries', () => {
       } )
       .expect( res => {
         expect( res.body ).not.toHaveProperty( 'errors' );
+        expect( res.body ).toHaveProperty( 'data' );
+        expect( res.body.data ).toHaveProperty( 'listUsers' );
+        expect( res.body.data.listUsers[ 0 ] ).toHaveProperty( '_id' );
+        expect( res.body.data.listUsers[ 0 ] ).toHaveProperty( 'uid' );
+        expect( res.body.data.listUsers[ 0 ] ).toHaveProperty( 'name' );
+        expect( res.body.data.listUsers[ 0 ] ).toHaveProperty( 'title' );
       } )
       .end( ( err, res ) => {
         done( err );
