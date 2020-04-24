bfabb3558f30f059dfabefc543b4383e70999406
Rigin Oommen <riginoommen@gmail.com>
2020-04-24 12:29:56 +0530

    Changed the directory for the yo executable

--- cli-init.sh
+++ cli-init.sh
@@ -2,4 +2,4 @@
 cd packages
 
 # Initate template QA
-../cli/generator-op-microservice/node_modules/yo/lib/cli.js --no-insight --no-update-notifier ../cli/generator-op-microservice/generators/microservice
+../cli/generator-op-microservice/node_modules/.bin/yo --no-insight --no-update-notifier ../cli/generator-op-microservice/generators/microservice
