1de0c33df102c87302b96b951725ce1fb82aff6a
Deepesh Nair <hybridx18@gmail.com>
2020-04-24 10:54:26 +0530

    feat(#196) - Remove the tabs from home page and change to linear structure (#197)
    * (feat#196) - Removed from tab and added in the page according to the designs and also updated the test cases
    
    * feat(#196) - Added margin in the two sections
    
    * feat(#196) - Removed extra div and added style in loader

--- packages/home-spa/src/js/controller.js
+++ packages/home-spa/src/js/controller.js
@@ -1,10 +1,16 @@
 // Services
 import { getData }  from './service';
 import { stub } from '../mocks/stub';
+// importing images
+import '../img/bell.svg';
+import '../img/shield.svg';
+import '../img/talk-bubble.svg';
+import '../img/login.svg';
+import '../img/magnifying-glass.svg';
 
 const microserviceCard = document.getElementById('microservice-cards');
-const spaList = document.getElementById('banner__links');
 const carouselSlide = document.getElementById('carousel-slide');
+const spaList = document.getElementById('banner__links');
 
 microserviceCard.innerHTML = `<div class="loader"></div>`;
 carouselSlide.innerHTML = `<div class="loader"></div>`;
