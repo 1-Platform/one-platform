5b076aca23bd743e9f3af77345c91c7da69db25b
Deepesh Nair <hybridx18@gmail.com>
2020-04-25 21:20:47 +0530

    feat(#200) - Microservice details page in home-spa (#201)
    * feat(#200) - Some basic optimizations and added ssi comment in the head of 404 ,microservice and index page
    
    * feat(#200) - Added header and microservice details
    
    * feat(#200) - Changed the id to lowercase and replaced spaces with - (hyphen)

--- packages/home-spa/src/js/controller.js
+++ packages/home-spa/src/js/controller.js
@@ -1,19 +1,27 @@
 // Services
 import { getData }  from './service';
-import { stub } from '../mocks/stub';
+import { stub, microserviceMock } from '../mocks/stub';
 // importing images
 import '../img/bell.svg';
 import '../img/shield.svg';
 import '../img/talk-bubble.svg';
 import '../img/login.svg';
 import '../img/magnifying-glass.svg';
+import '../img/Auth-illustration.svg';
+import '../img/Notification-illustration.svg';
+import '../img/User-profile-illustration.svg';
 
-const microserviceCard = document.getElementById('microservice-cards');
-const carouselSlide = document.getElementById('carousel-slide');
-const spaList = document.getElementById('banner__links');
+const microserviceCard = document.querySelector('#microservice-cards');
+const carouselSlide = document.querySelector('#carousel-slide');
+const spaList = document.querySelector('#banner-links');
+const footer = document.querySelector('footer');
+const band = document.querySelector('#band');
+const microserviceDetails = document.querySelector('#microservice-details');
 
-microserviceCard.innerHTML = `<div class="loader"></div>`;
-carouselSlide.innerHTML = `<div class="loader"></div>`;
+if (microserviceCard !== null && carouselSlide !== null) {
+  microserviceCard.innerHTML = `<div class="loader"></div>`;
+  carouselSlide.innerHTML = `<div class="loader"></div>`;
+}
 
 getData().then(
   (data) => {
@@ -92,5 +100,81 @@ function buildDom(apiData) {
       }
     }
     ).join('');
+  }
+
+  if (footer !== null) {
+    footer.innerHTML = `
+    <div class="container footer">
+    <div class="footer__links-col">
+      <div class="footer__links-header">
+        Quick links
+      </div>
+      <div class="footer__links">
+        <a href="#">One Portal in Mojo</a>
+        <a href="#">Weekly Blog</a>
+        <a href="#">Contact Us</a>
+        <a href="#">Changelog</a>
+        <a href="#">Subscribe to One Portal List</a>
+      </div>
+    </div>
+    <div class="footer__links-col">
+      <div class="footer__links-header">
+        Related sites
+      </div>
+      <div class="footer__links">
+        <a href="#">PnT Help</a>
+        <a href="#">PnT DevOps (Mojo)</a>
+        <a href="#">PnT Stats</a>
+        <a href="#">Projects Engineering</a>
+      </div>
+    </div>
+    <div class="footer__links-col">
+      <div class="footer__links-header">
+        Help
+      </div>
+      <div class="footer__links">
+        <a href="#">One Portal FAQs</a>
+        <a href="#">File an Issue</a>
+        <a href="mailto:one-portal-devel@redhat.com">one-portal-devel@redhat.com</a>
+      </div>
+    </div>
+  </div>
+  <div class="footer__copyright">
+    Copyright &copy; 2020 Red Hat Inc. Internal Use Only
+  </div>`;
+  }
+
+  if (band !== null) {
+    band.innerHTML = `
+    <div class="band__header">
+			Join the One Platform Digital Experience!
+		</div>
+    <button class="band__button--primary-border">Get Started</button>`;
+  }
+
+  if (microserviceDetails !== null) {
+    microserviceDetails.innerHTML = microserviceMock.map(microservice => {
+    return `<div class="microservice__details">
+    <div class="microservice__details--header" id="${microservice.title.replace(/\s+/g, '-').toLowerCase()}">${microservice.title}</div>
+    <div class="microservice__information">
+        <div class="microservice__section">
+            <div class="microservice__section--paragraph">
+                ${microservice.info}
+            </div>
+            <div class="microservice__section--supported-feaures">Supported features:</div>
+            <ul>
+            ${microservice.features.map(feature => {
+                return `<li>
+                          <em class="fa fa-check-circle"></em>
+                          <span>${feature}</span>
+                      </li>`}).join('')}
+            </ul>
+        </div>
+      <div class="microservice__image">
+        <img src="${microservice.illustration}" 
+      </div>
+      </div>
+      </div>`
+    }).join('');
   }
 }
--- packages/home-spa/src/mocks/stub.js
+++ packages/home-spa/src/mocks/stub.js
@@ -2,7 +2,7 @@ export const stub = [
    {
       name: "Authentication",
       description: "Adding authentication to your application is pice of cake. Just integrate with our API and focus on application development.",
-      link: "#",
+      link: "microservice-details.html#authentication",
       icon: "img/shield.svg",
       entityType: "microservice",
       colorScheme: "linear-gradient(119deg, #E2EAF6 0%, #E8E4F6 100%)",
@@ -11,7 +11,7 @@ export const stub = [
    {
       name: "Feedback",
       description: "Allows users to review and rate services and products provided by teams. Built into a power packed microservice so serve all.",
-      link: "#",
+      link: "microservice-details.html#feedback",
       icon: "img/talk-bubble.svg",
       entityType: "microservice",
       colorScheme: "linear-gradient(119deg, #E4F3F4 0%, #EDF6F0 100%)",
@@ -20,7 +20,7 @@ export const stub = [
    {
       name: "Notification",
       description: "Notification framework for One Platform and also all other SPAs",
-      link: "#",
+      link: "microservice-details.html#notification",
       icon: "img/bell.svg",
       entityType: "microservice",
       colorScheme: "linear-gradient(119deg, #FFF7C4 0%, #F2F7E6 100%)",
@@ -29,7 +29,7 @@ export const stub = [
    {
       name: "User Profile",
       description: "A prebuilt integration with LDAP to authenticate, authorize and get user information.",
-      link: "#",
+      link: "microservice-details.html#user-profile",
       icon: "img/login.svg",
       entityType: "microservice",
       colorScheme: "linear-gradient(119deg, #E4F3F4 0%, #F3F3F3 100%)",
@@ -38,7 +38,7 @@ export const stub = [
    {
       name: "Search",
       description: "A unified search API gives you ability to search across all hosted applications on the platform.",
-      link: "#",
+      link: "microservice-details.html#search",
       icon: "img/magnifying-glass.svg",
       entityType: "microservice",
       colorScheme: "linear-gradient(119deg, #FDDBDB 0%, #FDDBC7 100%)",
@@ -71,4 +71,42 @@ export const stub = [
       colorScheme: "",
       videoUrl: ""
    }
+];
+
+export const microserviceMock = [
+   {
+      title: 'Authentication',
+      info: 'Authentication microservices would verify and authenticate the legitimate user and grant necessary access.',
+      features: [
+         'SSO integration.',
+         'Authenticate SPA users using authentication microservice.',
+         'Work with your microservice to authorize users.'
+      ],
+      illustration: 'img/Auth-illustration.svg',
+   },
+   {
+      title: 'Notification',
+      info: 'Notification microservice is tightly coupled with pre-deployed SPA’s to support needs like custom notifications, subscriptions, control over notifications configuration etc. This microservice support both time-based and trigger-based notifications.',
+      features: [
+         'Manage & Support hosted SPA notifications.',
+         'Manage & Support notifications between SPA’s hosted over One Platform.',
+         'Manage SPA Subscriptions.',
+         'Notification UI to view SPA specific notifications, configuration management dashboard.',
+         'Integration with User & Group microservice to notify targetted users.',
+         'Integration with Hydra (near future).',
+      ],
+      illustration: 'img/Notification-illustration.svg',
+   },
+   {
+      title: 'User Profile',
+      info: 'User & Group microservice has been designed to handle and provide necessary user information to hosted SPA’s. The microservice is integrated with Rover to manage the groups and related membership.',
+      features: [
+         'Provide User & Group information.',
+         'Role/Group based access control.',
+         'Access permissions on the platform and hosted SPAs.',
+         'LDAP & Rover Integration',
+         'User Interface to view access & roles.',
+      ],
+      illustration: 'img/User-profile-illustration.svg',
+   },
 ];
