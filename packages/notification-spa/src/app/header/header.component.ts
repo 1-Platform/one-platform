f769fb3d3b78ebf6f0ba194ac91b2c009cbd8d66
hybridx <hybridx18@gmail.com>
2020-04-27 11:56:51 +0530

    feat(#130) - Changed the breadcrumb link

--- packages/notification-spa/src/app/header/header.component.ts
+++ packages/notification-spa/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'op-notification-header',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  manageBreadcrumb = false;
 
-  constructor() { }
+  constructor(
+    router: Router,
+  ) {
+    router.events.subscribe( (event) => {
+      if (event instanceof NavigationEnd) {
+        const urlSubdirectory = event.urlAfterRedirects.split('/')[1];
+        this.manageBreadcrumb = urlSubdirectory === 'manage' ? true : false;
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
