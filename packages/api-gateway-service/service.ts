288e6ca23324bfef327a27fa199b10a454116daf
Rigin Oommen <riginoommen@gmail.com>
2020-05-05 22:45:34 +0530

    feat:ONEPLAT-170 Authorization in middleware

--- packages/api-gateway-service/service.ts
+++ packages/api-gateway-service/service.ts
@@ -1,12 +1,13 @@
 import { ApolloGateway } from '@apollo/gateway';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer, AuthenticationError } from 'apollo-server-express';
 import express from 'express';
 import fs from 'fs';
 import http from 'http';
 import https from 'https';
-import { serviceList } from './src/helpers';
+import { serviceList, publicKey } from './src/helpers';
 import cookieParser = require( 'cookie-parser' );
 const { ApolloLogExtension } = require( 'apollo-log' );
+import { verify } from 'jsonwebtoken';
 
 /* Setting port for the server */
 const port = process.env.PORT || 4000;
@@ -41,14 +42,56 @@ const server = process.env.NODE_ENV !== 'test'
 ( async () => {
   const { schema, executor } = await apiGateway.load();
   /* Defining the Apollo Server */
+  let playgroundOptions: boolean | Object = {
+    title: 'API Gateway',
+    settings: {
+      'request.credentials': 'include'
+    }
+  };
+  if ( process.env.NODE_ENV === 'production' ) {
+    playgroundOptions = false;
+  }
+
+  /* Auth Token Verification Check */
+  app.post( '/graphql', ( req, res, next ) => {
+    try {
+      publicKey().then( ( key: string ) => {
+        console.log( req.headers.authorization );
+        if ( !req.headers.authorization ) {
+          throw new AuthenticationError( 'Auth Token Missing' );
+        }
+        const accessToken = req.headers.authorization?.split( ` ` )[ 1 ] || req.cookies[ 'access-token' ];
+        verify( accessToken, key, { algorithms: [ 'RS256' ] }, ( err: any, payload: any ) => {
+          if ( err && err.name === 'TokenExpiredError' ) {
+            return res.status( 401 ).json( err );
+          } else if ( err && err.name === 'JsonWebTokenError' ) {
+            return res.status( 401 ).json( err );
+          } else if ( !err ) {
+            next();
+          }
+        } );
+      } );
+    } catch ( err ) {
+      return res.status( 403 ).json( err );
+    }
+  } );
+
   const apollo = new ApolloServer( {
     schema: schema,
     executor: executor,
-    playground: process.env.NODE_ENV !== 'production',
-    subscriptions: false,
+    introspection: true,
+    context: ( { req, res }: any ) => ( { req, res } ),
+    formatError: error => ( {
+      message: error.message,
+      locations: error.locations,
+      stack: error.stack ? error.stack.split( '\n' ) : [],
+      path: error.path,
+    } ),
+    playground: playgroundOptions,
     tracing: process.env.NODE_ENV !== 'production',
   },
   );
+
   /* Applying apollo middleware to express server */
   apollo.applyMiddleware( { app } );
 } )();
--- packages/api-gateway-service/src/helpers.ts
+++ packages/api-gateway-service/src/helpers.ts
@@ -1,4 +1,17 @@
-/*This consists of the list of microservices which needs to be part of the gateway*/
+const fetch = require( 'node-fetch' );
+
+/* This consists of the list of microservices which needs to be part of the gateway */
 export const serviceList = [
   { name: 'User Group', url: process.env.USER_GROUP }
 ];
+
+/* Function to fetch the public key from internal IDP */
+export const publicKey = () => {
+  return fetch( process.env.INTERNAL_IDP )
+    .then( ( res: any ) => res.json() )
+    .then( ( res: any ) => {
+      return `-----BEGIN PUBLIC KEY-----
+${ res.public_key }
+-----END PUBLIC KEY-----`;
+    } );
+};
