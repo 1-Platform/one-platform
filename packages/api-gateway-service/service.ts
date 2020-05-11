c8580de8c8b53d8dbda73bb48e1eb15dbd5efa84
Rigin Oommen <riginoommen@gmail.com>
2020-05-11 10:46:18 +0530

    Included headers for the auth

--- packages/api-gateway-service/service.ts
+++ packages/api-gateway-service/service.ts
@@ -1,4 +1,4 @@
-import { ApolloGateway } from '@apollo/gateway';
+import { ApolloGateway, RemoteGraphQLDataSource } from '@apollo/gateway';
 import { ApolloServer, AuthenticationError } from 'apollo-server-express';
 import express from 'express';
 import fs from 'fs';
@@ -6,25 +6,30 @@ import http from 'http';
 import https from 'https';
 import { serviceList, publicKey } from './src/helpers';
 import cookieParser = require( 'cookie-parser' );
-const { ApolloLogExtension } = require( 'apollo-log' );
 import { verify } from 'jsonwebtoken';
+import { print } from 'graphql';
 
 /* Setting port for the server */
 const port = process.env.PORT || 4000;
 const app = express();
+let decodedPayload: any = '';
 
-/* Mount cookie parker */
+/* Mount cookie parser */
 app.use( cookieParser() );
 
-// Logging extension for gateway
-const loggingExtension = [ () => new ApolloLogExtension( {
-  level: 'info',
-  timestamp: true,
-} ) ];
+/* Option to set the custom headers with the request */
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  willSendRequest ( { request, context }: any ) {
+    request.http.headers.set( 'tokenPayload', decodedPayload );
+  }
+}
 
 /* Defining the Apollo Gateway */
 const apiGateway = new ApolloGateway( {
-  serviceList: serviceList
+  serviceList: serviceList,
+  buildService ( { name, url } ) {
+    return new AuthenticatedDataSource( { url } );
+  },
 } );
 
 /*  Creating the server based on the environment */
@@ -51,31 +56,6 @@ const server = process.env.NODE_ENV !== 'test'
   if ( process.env.NODE_ENV === 'production' ) {
     playgroundOptions = false;
   }
-
-  /* Auth Token Verification Check */
-  app.post( '/graphql', ( req, res, next ) => {
-    try {
-      publicKey().then( ( key: string ) => {
-        console.log( req.headers.authorization );
-        if ( !req.headers.authorization ) {
-          throw new AuthenticationError( 'Auth Token Missing' );
-        }
-        const accessToken = req.headers.authorization?.split( ` ` )[ 1 ] || req.cookies[ 'access-token' ];
-        verify( accessToken, key, { algorithms: [ 'RS256' ] }, ( err: any, payload: any ) => {
-          if ( err && err.name === 'TokenExpiredError' ) {
-            return res.status( 401 ).json( err );
-          } else if ( err && err.name === 'JsonWebTokenError' ) {
-            return res.status( 401 ).json( err );
-          } else if ( !err ) {
-            next();
-          }
-        } );
-      } );
-    } catch ( err ) {
-      return res.status( 403 ).json( err );
-    }
-  } );
-
   const apollo = new ApolloServer( {
     schema: schema,
     executor: executor,
@@ -89,12 +69,35 @@ const server = process.env.NODE_ENV !== 'test'
     } ),
     playground: playgroundOptions,
     tracing: process.env.NODE_ENV !== 'production',
-  },
-  );
+  } );
 
   /* Applying apollo middleware to express server */
   apollo.applyMiddleware( { app } );
 } )();
+
+/* Auth Token Verification Check */
+app.post( '/graphql', ( req, res, next ) => {
+  try {
+    publicKey().then( ( key: string ) => {
+      if ( !req.headers.authorization ) {
+        throw new AuthenticationError( 'Auth Token Missing' );
+      }
+      const accessToken = req.headers.authorization?.split( ` ` )[ 1 ] || req.cookies[ 'access-token' ];
+      verify( accessToken, key, { algorithms: [ 'RS256' ] }, ( err: any, payload: any ) => {
+        if ( err && err.name === 'TokenExpiredError' ) {
+          return res.status( 403 ).json( err );
+        } else if ( err && err.name === 'JsonWebTokenError' ) {
+          return res.status( 403 ).json( err );
+        } else if ( !err ) {
+          decodedPayload = JSON.stringify( payload );
+          next();
+        }
+      } );
+    } );
+  } catch ( err ) {
+    return res.status( 403 ).json( err );
+  }
+} );
 
 export default server.listen( { port: port }, () => {
   console.log( `Gateway Running on port ${ port }` );
