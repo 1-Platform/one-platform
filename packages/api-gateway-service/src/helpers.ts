497627663ff52f89dd68d64035c29f1b5e7da0a3
Rigin Oommen <riginoommen@gmail.com>
2020-05-14 13:58:10 +0530

    Converted the dependent microsevices api host format

--- packages/api-gateway-service/src/helpers.ts
+++ packages/api-gateway-service/src/helpers.ts
@@ -2,7 +2,7 @@ const fetch = require( 'node-fetch' );
 
 /* This consists of the list of microservices which needs to be part of the gateway */
 export const serviceList = [
-  { name: 'User Group', url: process.env.USER_GROUP }
+  { name: 'User Service', url: `http://${ process.env.USER_SERVICE_SERVICE_HOST }/graphql` },
 ];
 
 /* Function to fetch the public key from internal IDP */
