87b4ead7acfcbf2885befe6b2b8d563fe6f31ad5
Rigin Oommen <riginoommen@gmail.com>
2020-05-16 07:56:26 +0530

    feat(api): Included support for API query for additional parameters (#281)
    * feat-318 - Included support for API query as getUserBy
    
    * Included the missing test case
    
    * Update packages/user-service/src/e2e/user.spec.ts
    
    Co-authored-by: Mayur Deshmukh <deshmukhmayur@outlook.com>
    
    * Fixed the param
    
    Co-authored-by: Mayur Deshmukh <deshmukhmayur@outlook.com>

--- packages/user-service/src/e2e/user.spec.ts
+++ packages/user-service/src/e2e/user.spec.ts
@@ -20,15 +20,17 @@ const query = `
         ...userType
     }
   }
-  query GetUser($uid: String!) {
-    getUser(uid: $uid) {
-      ...userType
-    }
-  }
+
   mutation AddingUser($input: UserInput) {
       addUser(input: $input) {
           ...userType
       }
+  }
+
+  query getUsersBy($uid: String) {
+    getUsersBy(uid: $uid) {
+      ...userType
+    }
   }
 
   mutation UpdatingUser($input: UserInput) {
@@ -44,112 +46,135 @@ const query = `
   }
 `;
 
-beforeAll(() => {
-  request = supertest.agent(User);
-});
-afterAll(done => {
-  return User.close(done);
-});
+beforeAll( () => {
+  request = supertest.agent( User );
+} );
+afterAll( done => {
+  return User.close( done );
+} );
 
-describe('User-Group Microservice API Test', () => {
-  it('should create a new User', done => {
+describe( 'User-Group Microservice API Test', () => {
+  it( 'should create a new User', done => {
     request
-      .post('/graphql')
-      .send({
+      .post( '/graphql' )
+      .send( {
         query: query,
         operationName: 'AddingUser',
         variables: {
           input: mock
         }
-      })
-      .expect(res => {
-        expect(res.body).not.toHaveProperty('errors');
-        expect(res.body).toHaveProperty('data');
-        expect(res.body.data).toHaveProperty('addUser');
-        expect(res.body.data.addUser).toHaveProperty('_id', mock._id);
-        expect(res.body.data.addUser).toHaveProperty('uid', mock.uid);
-        expect(res.body.data.addUser).toHaveProperty('name', mock.name);
-        expect(res.body.data.addUser).toHaveProperty('title', mock.title);
-      })
-      .end((err, res) => {
-        done(err);
-      });
-  });
+      } )
+      .expect( res => {
+        expect( res.body ).not.toHaveProperty( 'errors' );
+        expect( res.body ).toHaveProperty( 'data' );
+        expect( res.body.data ).toHaveProperty( 'addUser' );
+        expect( res.body.data.addUser ).toHaveProperty( '_id', mock._id );
+        expect( res.body.data.addUser ).toHaveProperty( 'uid', mock.uid );
+        expect( res.body.data.addUser ).toHaveProperty( 'name', mock.name );
+        expect( res.body.data.addUser ).toHaveProperty( 'title', mock.title );
+      } )
+      .end( ( err, res ) => {
+        done( err );
+      } );
+  } );
 
-  it('should list Users', (done) => {
+  it( 'should get Users by uid', ( done ) => {
     request
-      .post('/graphql')
-      .send({
+      .post( '/graphql' )
+      .send( {
+        query: query,
+        operationName: 'getUsersBy',
+        variables: {
+          uid: mock.uid
+        }
+      } )
+      .expect( res => {
+        expect( res.body ).not.toHaveProperty( 'errors' );
+        expect( res.body ).toHaveProperty( 'data' );
+        expect( res.body.data ).toHaveProperty( 'getUsersBy' );
+        expect( res.body.data.getUsersBy ).not.toHaveLength( 0 );
+        expect( res.body.data.getUsersBy[ 0 ] ).toHaveProperty( '_id' );
+        expect( res.body.data.getUsersBy[ 0 ] ).toHaveProperty( 'uid' );
+        expect( res.body.data.getUsersBy[ 0 ] ).toHaveProperty( 'name' );
+        expect( res.body.data.getUsersBy[ 0 ] ).toHaveProperty( 'title' );
+      } )
+      .end( ( err, res ) => {
+        done( err );
+      } );
+  } );
+
+  it( 'should list all Users', ( done ) => {
+    request
+      .post( '/graphql' )
+      .send( {
         query: query,
         operationName: 'ListUsers',
         variables: {
           uid: mock.uid
         }
-      })
-      .expect(res => {
-        expect(res.body).not.toHaveProperty('errors');
-        expect(res.body).toHaveProperty('data');
-        expect(res.body.data).toHaveProperty('listUsers');
-        expect(res.body.data.listUsers).not.toHaveLength(0);
-        expect(res.body.data.listUsers[0]).toHaveProperty('_id');
-        expect(res.body.data.listUsers[0]).toHaveProperty('uid');
-        expect(res.body.data.listUsers[0]).toHaveProperty('name');
-        expect(res.body.data.listUsers[0]).toHaveProperty('title');
-      })
-      .end((err, res) => {
-        done(err);
-      });
-  });
+      } )
+      .expect( res => {
+        expect( res.body ).not.toHaveProperty( 'errors' );
+        expect( res.body ).toHaveProperty( 'data' );
+        expect( res.body.data ).toHaveProperty( 'listUsers' );
+        expect( res.body.data.listUsers ).not.toHaveLength( 0 );
+        expect( res.body.data.listUsers[ 0 ] ).toHaveProperty( '_id' );
+        expect( res.body.data.listUsers[ 0 ] ).toHaveProperty( 'uid' );
+        expect( res.body.data.listUsers[ 0 ] ).toHaveProperty( 'name' );
+        expect( res.body.data.listUsers[ 0 ] ).toHaveProperty( 'title' );
+      } )
+      .end( ( err, res ) => {
+        done( err );
+      } );
+  } );
 
-  it('should update the User', (done) => {
+  it( 'should update the User', ( done ) => {
     request
-      .post('/graphql')
-      .send({
+      .post( '/graphql' )
+      .send( {
         query: query,
         operationName: 'UpdatingUser',
         variables: {
           input: mock
         }
-      })
-      .expect(res => {
-        expect(res.body).not.toHaveProperty('errors');
-        expect(res.body).toHaveProperty('data');
+      } )
+      .expect( res => {
+        expect( res.body ).not.toHaveProperty( 'errors' );
+        expect( res.body ).toHaveProperty( 'data' );
 
-        expect(res.body.data).toHaveProperty('updateUser');
-        expect(res.body.data.updateUser).toHaveProperty('_id', mock._id);
-        expect(res.body.data.updateUser).toHaveProperty('uid', mock.uid);
-        expect(res.body.data.updateUser).toHaveProperty('name', mock.name);
-        expect(res.body.data.updateUser).toHaveProperty('title', mock.title);
-      })
-      .end((err, res) => {
-        done(err);
-      });
-  });
+        expect( res.body.data ).toHaveProperty( 'updateUser' );
+        expect( res.body.data.updateUser ).toHaveProperty( '_id', mock._id );
+        expect( res.body.data.updateUser ).toHaveProperty( 'uid', mock.uid );
+        expect( res.body.data.updateUser ).toHaveProperty( 'name', mock.name );
+        expect( res.body.data.updateUser ).toHaveProperty( 'title', mock.title );
+      } )
+      .end( ( err, res ) => {
+        done( err );
+      } );
+  } );
 
-  it('should delete the user', (done) => {
+  it( 'should delete the user', ( done ) => {
     request
-      .post('/graphql')
-      .send({
+      .post( '/graphql' )
+      .send( {
         query: query,
         operationName: 'DeletingUser',
         variables: {
           _id: mock._id
         }
-      })
-      .expect(res => {
-        expect(res.body).not.toHaveProperty('errors');
-        expect(res.body).toHaveProperty('data');
+      } )
+      .expect( res => {
+        expect( res.body ).not.toHaveProperty( 'errors' );
+        expect( res.body ).toHaveProperty( 'data' );
 
-        expect(res.body.data).toHaveProperty('deleteUser');
-        expect(res.body.data.deleteUser).toHaveProperty('_id', mock._id);
-        expect(res.body.data.deleteUser).toHaveProperty('uid', mock.uid);
-        expect(res.body.data.deleteUser).toHaveProperty('name', mock.name);
-        expect(res.body.data.deleteUser).toHaveProperty('title', mock.title);
-      })
-      .end((err, res) => {
-        done(err);
-      });
-  });
-});
-
-
+        expect( res.body.data ).toHaveProperty( 'deleteUser' );
+        expect( res.body.data.deleteUser ).toHaveProperty( '_id', mock._id );
+        expect( res.body.data.deleteUser ).toHaveProperty( 'uid', mock.uid );
+        expect( res.body.data.deleteUser ).toHaveProperty( 'name', mock.name );
+        expect( res.body.data.deleteUser ).toHaveProperty( 'title', mock.title );
+      } )
+      .end( ( err, res ) => {
+        done( err );
+      } );
+  } );
+} );
--- packages/user-service/src/resolver.ts
+++ packages/user-service/src/resolver.ts
@@ -5,94 +5,98 @@ import { UserGroupAPIHelper } from './helpers';
 
 export const UserGroupResolver = {
   Query: {
-    getUser(root: any, args: any, ctx: any) {
-      return User.findOne({ uid: args.uid })
-        .then(data => data)
-        .catch(err => err);
+    getUsersBy ( root: any, args: any, ctx: any ) {
+      const keyParam = ( Object.keys( args )[ 0 ] ).toString();
+      const valueParam = Object.values( args )[ 0 ];
+      const query: any = {};
+      query[ keyParam ] = valueParam;
+      return User.find( query )
+        .then( data => data )
+        .catch( err => err );
     },
-    listUsers(root: any, args: any, ctx: any) {
+    listUsers ( root: any, args: any, ctx: any ) {
       return User.find()
-        .then(data => data)
-        .catch(err => err);
+        .then( data => data )
+        .catch( err => err );
     },
-    getGroupMembers(root: any, args: any, ctx: any){
+    getGroupMembers ( root: any, args: any, ctx: any ) {
       const groupMembers: any = [];
-      return UserGroupAPIHelper.getProfilesBy(`(cn=${args.cn})`)
-        .then((response: any) => {
-          response.uniqueMember.map((member: any) => {
-            groupMembers.push(member.substring(member.indexOf('uid=') + 4, member.indexOf(',')));
-          });
-        })
-        .then(() => {
-          return groupMembers.map((user: any, index: any) => {
-            return UserGroupAPIHelper.getProfilesBy(`(uid=${user})`)
-              .catch(err => {
-                console.log('error', err);
+      return UserGroupAPIHelper.getProfilesBy( `(cn=${ args.cn })` )
+        .then( ( response: any ) => {
+          response.uniqueMember.map( ( member: any ) => {
+            groupMembers.push( member.substring( member.indexOf( 'uid=' ) + 4, member.indexOf( ',' ) ) );
+          } );
+        } )
+        .then( () => {
+          return groupMembers.map( ( user: any, index: any ) => {
+            return UserGroupAPIHelper.getProfilesBy( `(uid=${ user })` )
+              .catch( err => {
+                console.log( 'error', err );
                 throw err;
-              });
-          });
-        })
-        .then(userPromise => Promise.all(userPromise))
-        .then(users => _.compact(users))
-        .catch(err => {
+              } );
+          } );
+        } )
+        .then( userPromise => Promise.all( userPromise ) )
+        .then( users => _.compact( users ) )
+        .catch( err => {
           throw err;
-        });
+        } );
     }
   },
   Mutation: {
-    addUser(root: any, args: any, ctx: any) {
-      const data = new User(args.input);
+    addUser ( root: any, args: any, ctx: any ) {
+      const data = new User( args.input );
       data.isActive = true;
       return data.save()
-        .then(response => response)
-        .catch(err => err);
+        .then( response => response )
+        .catch( err => err );
     },
-    addUserFromLDAP(root: any, args: any, ctx: any) {
-      return User.findOne({ uid: args.uid })
-        .then((data: any) => {
-          if (!data) {
-            return UserGroupAPIHelper.getProfilesBy(`(uid=${args.uid})`).then((response: any) => {
-              if (!_.isEmpty(response)) {
+    addUserFromLDAP ( root: any, args: any, ctx: any ) {
+      return User.findOne( { uid: args.uid } )
+        .then( ( data: any ) => {
+          if ( !data ) {
+            return UserGroupAPIHelper.getProfilesBy( `(uid=${ args.uid })` ).then( ( response: any ) => {
+              if ( !_.isEmpty( response ) ) {
                 const groups: any = [];
-                response.memberOf.map((group: any) => {
-                  groups.push(group.substring(group.indexOf(`cn=`) + 3, group.indexOf(`,`)));
-                });
-                const newUser = new User({
+                response.memberOf.map( ( group: any ) => {
+                  groups.push( group.substring( group.indexOf( `cn=` ) + 3, group.indexOf( `,` ) ) );
+                } );
+                const newUser = new User( {
                   uid: response.uid,
                   name: response.cn,
                   rhatUUID: response.rhatUUID,
                   title: response.title,
                   memberOf: groups,
                   isActive: true,
-                  apiRole: (groups.includes('one-portal-devel')) ? `ADMIN` : 'USER',
+                  apiRole: ( groups.includes( 'one-portal-devel' ) ) ? `ADMIN` : 'USER',
                   timestamp: {
-                    createdAt: moment.utc(new Date())
+                    createdAt: moment.utc( new Date() )
                   }
-                });
+                } );
                 return newUser.save();
               } else {
                 return null;
               }
-            });
-          } else if (data) {
+            } );
+          } else if ( data ) {
             return data;
           }
-        })
-        .catch(err => err);
+        } )
+        .catch( err => err );
     },
-    deleteUser(root: any, args: any, ctx: any) {
-      return User.findByIdAndRemove(args._id)
-        .then(response => response)
-        .catch(err => err);
+    deleteUser ( root: any, args: any, ctx: any ) {
+      return User.findByIdAndRemove( args._id )
+        .then( response => response )
+        .catch( err => err );
     },
-    updateUser(root: any, args: any, ctx: any) {
-      return User.findById(args.input._id)
-        .then(response => {
-          return Object.assign(response, args.input)
+    updateUser ( root: any, args: any, ctx: any ) {
+      return User.findById( args.input._id )
+        .then( response => {
+          return Object.assign( response, args.input )
             .save()
-            .then((user: any) => user);
-        })
-        .catch((err: any) => err);
+            .then( ( user: any ) => user );
+        } )
+        .catch( ( err: any ) => err );
     }
   }
 };
