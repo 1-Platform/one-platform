d19ea20da63bad5a5e1cf87a04f37a78470f68bf
Rigin Oommen <riginoommen@gmail.com>
2020-05-16 07:49:39 +0530

    fix - 325 Converted the dependent microsevices api host format supported for openshift (#287)
    * Converted the dependent microsevices api host format
    
    * Included home and feedback services
    
    * Changed the URL type
    
    * Included the upcoming services

--- packages/api-gateway-service/src/helpers.ts
+++ packages/api-gateway-service/src/helpers.ts
@@ -2,7 +2,10 @@ const fetch = require( 'node-fetch' );
 
 /* This consists of the list of microservices which needs to be part of the gateway */
 export const serviceList = [
-  { name: 'User Group', url: process.env.USER_GROUP }
+  { name: 'User Service', url: `http://${ process.env.USER_SERVICE_SERVICE_HOST }/graphql` },
+  { name: 'Home Service', url: `http://${ process.env.HOME_SERVICE_SERVICE_HOST }/graphql` },
+  { name: 'Feedback Service', url: `http://${ process.env.FEEDBACK_SERVICE_SERVICE_HOST }/graphql` },
+  { name: 'Notification Service', url: `http://${ process.env.NOTIFICATION_SERVICE_SERVICE_HOST }/graphql` },
 ];
 
 /* Function to fetch the public key from internal IDP */
