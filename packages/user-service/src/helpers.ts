156df9a5424dfbe3e5f0a7ccd56049f87cd1c2e9
Rigin Oommen <riginoommen@gmail.com>
2020-05-20 21:29:24 +0530

    Included the helper function for the ldap and user bridge

--- packages/user-service/src/helpers.ts
+++ packages/user-service/src/helpers.ts
@@ -1,4 +1,7 @@
 import { createClient } from 'ldapjs';
+import { User } from './schema';
+import * as _ from 'lodash';
+import moment from 'moment';
 
 class UserGroupApiHelper {
   private static UserGroupHelperInstance: UserGroupApiHelper;
@@ -35,7 +38,34 @@ class UserGroupApiHelper {
       });
     });
   }
+
+  // Helper function for the database interaction with ldap
+  public addUserLDAP ( profile_param: string ) {
+    return new Promise( ( resolve, reject ) => {
+      this.getProfilesBy( profile_param ).then( ( response: any ) => {
+        if ( !_.isEmpty( response ) ) {
+          const groups: any = [];
+          response.memberOf.map( ( group: any ) => {
+            groups.push( group.substring( group.indexOf( `cn=` ) + 3, group.indexOf( `,` ) ) );
+          } );
+          const newUser = new User( {
+            uid: response.uid,
+            name: response.cn,
+            rhatUUID: response.rhatUUID,
+            title: response.title,
+            memberOf: groups,
+            isActive: true,
+            apiRole: ( groups.includes( 'one-portal-devel' ) ) ? `ADMIN` : 'USER',
+            createdBy: response.rhatUUID,
+            createdOn: moment.utc( new Date() )
+          } );
+          resolve( newUser.save() );
+        } else {
+          resolve( {} );
+        }
+      } );
+    } );
+  }
 }
 
 export const UserGroupAPIHelper = UserGroupApiHelper.getApiInstance();
-
