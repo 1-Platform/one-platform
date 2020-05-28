bfa9d8226040dd3015aa9bb5725d6a745d5b6cb0
anjsharm <anjsharm@redhat.com>
2020-05-28 13:43:43 +0530

    Update requested change

--- packages/feedback-spa/src/app/apollo.config.ts
+++ packages/feedback-spa/src/app/apollo.config.ts
@@ -7,7 +7,6 @@ import { environment } from '../environments/environment';
 import { onError } from 'apollo-link-error';
 import { ApolloLink, split } from 'apollo-link';
 import { RetryLink } from 'apollo-link-retry';
-import { WebSocketLink } from 'apollo-link-ws';
 import { getMainDefinition } from 'apollo-utilities';
 
 @NgModule({
@@ -18,14 +17,53 @@ import { getMainDefinition } from 'apollo-utilities';
   ]
 })
 export class GraphQLModule {
-  authorization = environment.authorization;
+  authorization = window.OpAuthHelper?.jwtToken
+  ? 'Bearer ' + window.OpAuthHelper.jwtToken
+  : '';
   header = new HttpHeaders().append('Authorization', this.authorization);
   constructor(apollo: Apollo, httpLink: HttpLink) {
 
     const uri = environment.graphqlAPI;
     const httpClient = httpLink.create({ uri, headers: this.header });
+
+    const retry = new RetryLink({
+      delay: {
+        initial: 500,
+        max: Infinity,
+        jitter: false
+      },
+      attempts: {
+        max: 5,
+        retryIf: (_error, _operation) => !!_error
+      }
+    });
+
+    const error = onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${JSON.stringify(message)}, Location: ${JSON.stringify(locations)}, Path: ${JSON.stringify(path)}`,
+          ),
+        );
+      }
+      if (networkError) {
+          console.log(`[Network error]: ${JSON.stringify(networkError)}`);
+      }
+    });
+
+    const omitTypename = (key, value) => (key === '__typename' ? undefined : value);
+    const cleanTypeName = new ApolloLink((operation, forward) => {
+      if (operation.variables) {
+        operation.variables = JSON.parse(JSON.stringify(operation.variables), omitTypename);
+      }
+      return forward(operation).map((data) => {
+        return data;
+      });
+    });
+
+    const link = ApolloLink.from([cleanTypeName, retry, error, httpClient]);
     apollo.create({
-      link: httpClient,
+      link,
       cache: new InMemoryCache(),
       defaultOptions: {
         watchQuery: {
