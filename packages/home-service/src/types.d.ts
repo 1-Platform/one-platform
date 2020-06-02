5c11b91e59013c20a77f899d7fa315bd5d7588e0
hybridx <hybridx18@gmail.com>
2020-06-02 16:15:24 +0530

    fix(#360) - Added enum and fixed space for type.d.ts and type.graphql

--- packages/home-service/src/types.d.ts
+++ packages/home-service/src/types.d.ts
@@ -1,35 +1,39 @@
 declare module '*.graphql';
 declare module '*.json';
 
+declare enum Role {
+  admin = "ADMIN",
+  user = "USER"
+}
 type PermissionsType = {
   roverGroup: string;
-  role: string;
+  role: Role;
 }
 type UserType = {
-    name: string;
-    title: string;
-    uid: string;
-    rhatUUID: string;
-    isActive ?: boolean;
-    memberOf: string[];
-    apiRole: string,
-    createdBy: string;
-    createdOn: Date;
-    updatedBy: string;
-    updatedOn: Date;
+  name: string;
+  title: string;
+  uid: string;
+  rhatUUID: string;
+  isActive ?: boolean;
+  memberOf: string[];
+  apiRole: string,
+  createdBy: string;
+  createdOn: Date;
+  updatedBy: string;
+  updatedOn: Date;
   }
 type HomeType = {
-    name: string;
-    description: string;
-    link: string;
-    icon: string;
-    entityType: string;
-    colorScheme: string;
-    videoUrl: string;
-    owners: string[] | UserType[];
-    createdBy: string  | UserType;
-    createdOn: Date;
-    updatedBy: string | UserType;
-    updatedOn: Date;
-    permissions: PermissionsType[];
+  name: string;
+  description: string;
+  link: string;
+  icon: string;
+  entityType: string;
+  colorScheme: string;
+  videoUrl: string;
+  owners: string[] | UserType[];
+  createdBy: string  | UserType;
+  createdOn: Date;
+  updatedBy: string | UserType;
+  updatedOn: Date;
+  permissions: PermissionsType[];
 }
