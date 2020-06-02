10e912414b17e02929d17e9b39faeaf3e703299f
ghanlohar <gslohar@gmail.com>
2020-06-02 16:41:36 +0530

    OnePlat-333-Adding owner array to source.

--- packages/notifications-service/src/helpers.ts
+++ packages/notifications-service/src/helpers.ts
@@ -1,3 +1,29 @@
 // write your helper functions here
-import { RedisPubSub } from 'graphql-redis-subscriptions';
-export const pubsub = new RedisPubSub();
+import fetch from "node-fetch";
+
+class NotificationAPIHelper {
+  buildGqlQuery(response: NotificationsConfigType[]) {
+    return response
+      .map(
+        (
+          notificationsConfig
+        ) => `source_${notificationsConfig.source}: getHomeType( _id: "${notificationsConfig.source}" ){
+              owners
+            }`
+      )
+      .join();
+  }
+
+  getSourceDetails(query: string) {
+    return fetch(
+      `http://home-service.one-platform.int.open.paas.redhat.com/graphql`,
+      {
+        method: "post",
+        body: JSON.stringify({ query: query }),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then((res: any) => res.json());
+  }
+}
+
+export const NotificationHelper = new NotificationAPIHelper();
