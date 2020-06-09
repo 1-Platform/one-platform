ba11b9090b42dc7aee1b996150264f0a067fd384
Anjnee K. Sharma <anjnee.k.sharma@gmail.com>
2020-06-09 13:35:41 +0530

    Add base url for feedback (#408)
    Co-authored-by: anjsharm <anjsharm@redhat.com>

--- packages/feedback-spa/src/app/apollo.config.ts
+++ packages/feedback-spa/src/app/apollo.config.ts
@@ -9,24 +9,24 @@ import { ApolloLink, split } from 'apollo-link';
 import { RetryLink } from 'apollo-link-retry';
 import { getMainDefinition } from 'apollo-utilities';
 
-@NgModule({
+@NgModule( {
   exports: [
     HttpClientModule,
     ApolloModule,
     HttpLinkModule
   ]
-})
+} )
 export class GraphQLModule {
-  authorization = window.OpAuthHelper?.jwtToken
-  ? 'Bearer ' + window.OpAuthHelper.jwtToken
-  : '';
-  header = new HttpHeaders().append('Authorization', this.authorization);
-  constructor(apollo: Apollo, httpLink: HttpLink) {
+  authorization = ( window as any ).OpAuthHelper?.jwtToken
+    ? 'Bearer ' + ( window as any ).OpAuthHelper.jwtToken
+    : '';
+  header = new HttpHeaders().append( 'Authorization', this.authorization );
+  constructor ( apollo: Apollo, httpLink: HttpLink ) {
 
     const uri = environment.graphqlAPI;
-    const httpClient = httpLink.create({ uri, headers: this.header });
+    const httpClient = httpLink.create( { uri, headers: this.header } );
 
-    const retry = new RetryLink({
+    const retry = new RetryLink( {
       delay: {
         initial: 500,
         max: Infinity,
@@ -34,35 +34,35 @@ export class GraphQLModule {
       },
       attempts: {
         max: 5,
-        retryIf: (_error, _operation) => !!_error
+        retryIf: ( _error, _operation ) => !!_error
       }
-    });
+    } );
 
-    const error = onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors) {
-        graphQLErrors.map(({ message, locations, path }) =>
+    const error = onError( ( { graphQLErrors, networkError } ) => {
+      if ( graphQLErrors ) {
+        graphQLErrors.map( ( { message, locations, path } ) =>
           console.log(
-            `[GraphQL error]: Message: ${JSON.stringify(message)}, Location: ${JSON.stringify(locations)}, Path: ${JSON.stringify(path)}`,
+            `[GraphQL error]: Message: ${ JSON.stringify( message ) }, Location: ${ JSON.stringify( locations ) }, Path: ${ JSON.stringify( path ) }`,
           ),
         );
       }
-      if (networkError) {
-          console.log(`[Network error]: ${JSON.stringify(networkError)}`);
+      if ( networkError ) {
+        console.log( `[Network error]: ${ JSON.stringify( networkError ) }` );
       }
-    });
+    } );
 
-    const omitTypename = (key, value) => (key === '__typename' ? undefined : value);
-    const cleanTypeName = new ApolloLink((operation, forward) => {
-      if (operation.variables) {
-        operation.variables = JSON.parse(JSON.stringify(operation.variables), omitTypename);
+    const omitTypename = ( key, value ) => ( key === '__typename' ? undefined : value );
+    const cleanTypeName = new ApolloLink( ( operation, forward ) => {
+      if ( operation.variables ) {
+        operation.variables = JSON.parse( JSON.stringify( operation.variables ), omitTypename );
       }
-      return forward(operation).map((data) => {
+      return forward( operation ).map( ( data ) => {
         return data;
-      });
-    });
+      } );
+    } );
 
-    const link = ApolloLink.from([cleanTypeName, retry, error, httpClient]);
-    apollo.create({
+    const link = ApolloLink.from( [ cleanTypeName, retry, error, httpClient ] );
+    apollo.create( {
       link,
       cache: new InMemoryCache(),
       defaultOptions: {
@@ -71,6 +71,6 @@ export class GraphQLModule {
           errorPolicy: 'all'
         }
       }
-    });
+    } );
   }
 }
