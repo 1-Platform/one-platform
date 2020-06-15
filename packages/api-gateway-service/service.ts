dff56339c2a2d32af5bb33e7160c0a60f7b71c3f
Rigin Oommen <riginoommen@gmail.com>
2020-06-15 17:54:40 +0530

    fix:371 Updated the Cors options to default (#444)
    * Updated the Cors options
    
    * Removed the credentials config
    
    * fix: Changed the corsoptions to default

--- packages/api-gateway-service/service.ts
+++ packages/api-gateway-service/service.ts
@@ -11,20 +11,11 @@ import cookieParser = require( 'cookie-parser' );
 const port = process.env.PORT || 4000;
 const app = express();
 
-// Configure CORS
-const corsOptions: cors.CorsOptions = {
-  allowedHeaders: [ 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token' ],
-  credentials: true,
-  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-  origin: '*',
-  preflightContinue: false
-};
-
 /* Mount cookie parser */
 app.use( cookieParser() );
 
 /* include cors middleware */
-app.use( cors( corsOptions ) );
+app.use( cors() );
 
 /*  Creating the server based on the environment */
 const server = http.createServer( app );
