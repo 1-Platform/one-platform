a417531f1c5e56dd8c6329759686b4738a7f153b
Sayak Sarkar <sayak.bugsmith@gmail.com>
2020-08-10 15:09:38 +0530

    docs(#587): Add OP CLI Documentation.

--- packages/documentation-spa/sidebars.js
+++ packages/documentation-spa/sidebars.js
@@ -1,5 +1,6 @@
 module.exports = {
   someSidebar: {
+    CLI: [ 'cli/op-cli' ],
     Components: [ 'components/component-library' ],
     Documentation: ['documentation/styleguide'],
     Miscelleaneous: ['miscellaneous/assets'],
