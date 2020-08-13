30611529febf2d8d5f9fbebca0f9fa2fb150c288
Mayur Deshmukh <deshmukhmayur@outlook.com>
2020-08-13 14:59:11 +0530

    Patch for auth middleware for Subscriptions
    - Also removed network call for Keycloak pubkey, and used env variable
    - And some code cleanup

--- packages/api-gateway-service/src/helpers.ts
+++ packages/api-gateway-service/src/helpers.ts
@@ -53,12 +53,29 @@ export async function getRemoteSchema ( { uri, subscriptionsUri }: any ) {
 }
 
 /* Function to fetch the public key from internal IDP */
-export const publicKey = () => {
-  return fetch( process.env.INTERNAL_IDP )
-    .then( ( res: any ) => res.json() )
-    .then( ( res: any ) => {
-      return `-----BEGIN PUBLIC KEY-----
-${ res.public_key }
------END PUBLIC KEY-----`;
-    } );
+const formatAsPem = (str: any) => {
+  const keyHeader = '-----BEGIN PUBLIC KEY-----';
+  const keyFooter = '-----END PUBLIC KEY-----';
+  let formatKey = '';
+  if ( str.startsWith( keyHeader ) && str.endsWith( keyFooter ) ) {
+    return str;
+  }
+
+  if ( str.split( '\n' ).length === 1 ) {
+    while ( str.length > 0 ) {
+      formatKey += `${ str.substring( 0, 64 ) }\n`;
+      str = str.substring( 64 );
+    }
+  }
+
+  return `${ keyHeader }\n${ formatKey }${ keyFooter }`;
+};
+
+export function getPublicKey(): string {
+  const publicKey = process.env.KEYCLOAK_PUBKEY;
+
+  if ( publicKey && publicKey.trim().length > 0 ) {
+    return formatAsPem( publicKey );
+  }
+  throw Error( 'No Keycloak Public Key found! Please configure it by setting the KEYCLOAK_PUBKEY environment variable.' );
 };
