b8240d8c039db1e2bfab8a2e38e894d29043df10
hybridx <hybridx18@gmail.com>
2020-09-17 19:16:40 +0530

    fix(#659) - Feedback header and footer added

--- packages/feedback-spa/src/app/app.component.ts
+++ packages/feedback-spa/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -6,6 +6,50 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: []
 })
-export class AppComponent {
-  title = 'feedback';
+export class AppComponent implements OnInit {
+  title = 'Feedback';
+  footerLinks = [
+    {
+      category: 'Quick Links',
+      links: [
+        { text: 'One Platform in Mojo', href: 'https://mojo.redhat.com/groups/pnt-devops/projects/one-portal/' },
+        { text: 'Weekly Blog', href: 'https://mojo.redhat.com/groups/pnt-devops/projects/one-portal/blog/' },
+        { text: 'Contact Us', href: '/contact-us' },
+      ]
+    },
+    {
+      category: 'Related sites',
+      links: [
+        { text: 'access.redhat.com', href: 'https://access.redhat.com/' },
+        { text: 'catalog.redhat.com', href: 'https://catalog.redhat.com/' },
+        { text: 'connect.redhat.com', href: 'https://connect.redhat.com/' },
+      ]
+    },
+    {
+      category: 'Help',
+      links: [
+        { text: 'Report An Issue' },
+        { text: 'One Portal FAQs', href: 'https://mojo.redhat.com/docs/DOC-1225598' },
+        { text: 'one-portal@redhat.com', href: 'mailto:one-portal@redhat.com' },
+        { text: 'GitHub', href: 'https://github.com/1-platform/one-platform' }
+      ]
+    },
+  ];
+  breadcrumbs = [
+    { name: 'Home', href: '/' },
+    { name: 'Feedback', href: '/feedback' },
+  ];
+  headerLinks = [
+    {name: 'Documentation', href: '/get-started/docs/microservices/feedback/feedback-service', icon: 'fa-file'}
+  ];
+
+  ngOnInit() {
+    const footer: any = document.querySelector('opc-footer');
+    footer.opcLinkCategories = this.footerLinks;
+    footer.addEventListener( 'opc-footer-link:click', () => {
+      (document.querySelector('op-feedback') as any).togglePanelVisibility();
+    }, false);
+    (document.querySelector('#breadcrumb') as any).opcHeaderBreadcrumb = this.breadcrumbs;
+    (document.querySelector('#links') as any).opcHeaderLinks = this.headerLinks;
+  }
 }
