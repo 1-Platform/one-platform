e58802987d8ca70b5609d847c87ff905811db8c9
hybridx <hybridx18@gmail.com>
2020-09-21 20:00:28 +0530

    fix(#659) - Added new config for new apollo graphql support

--- packages/notification-spa/src/app/graphql.module.ts
+++ packages/notification-spa/src/app/graphql.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { ApolloModule, Apollo, APOLLO_OPTIONS } from 'apollo-angular';
-import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloLink } from 'apollo-link';
-import { setContext } from 'apollo-link-context';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache, ApolloLink } from '@apollo/client/core';
+import { setContext } from '@apollo/client/link/context';
 import { environment } from 'src/environments/environment';
 
 const uri = environment.API_URL;
@@ -15,7 +14,6 @@ export function provideApollo(httpLink: HttpLink) {
       Accept: 'charset=utf-8'
     }
   }));
-
   const auth = setContext((operation, context) => ({
     headers: {
       Authorization: window.OpAuthHelper?.jwtToken
@@ -23,21 +21,16 @@ export function provideApollo(httpLink: HttpLink) {
       : '',
     },
   }));
-
-  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })]);
+  const link = ApolloLink.from( [ basic, auth, httpLink.create( { uri } ) ] );
   const cache = new InMemoryCache();
-
   return {
     link,
     cache
   };
 }
-
 @NgModule({
   exports: [
     HttpClientModule,
-    ApolloModule,
-    HttpLinkModule
   ],
   providers: [{
     provide: APOLLO_OPTIONS,
