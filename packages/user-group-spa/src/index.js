5c9fa6ae1ed44ea17e79794a7daaad401fba757f
Ghan Lohar <gslohar@gmail.com>
2020-11-25 13:30:07 +0530

    Fix: Adding check to initialise app after successful login. (#802)
    * Adding check to initialize app after successful login.
    
    * Updated code

--- packages/user-group-spa/src/index.js
+++ packages/user-group-spa/src/index.js
@@ -8,14 +8,18 @@ import * as serviceWorker from './serviceWorker';
 
 import BreadcrumbContextProvider from './context/BreadcrumbContext.jsx'
 
-render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <BreadcrumbContextProvider>
-      <App />
-    </BreadcrumbContextProvider>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+const renderApp = () => {
+  render(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BreadcrumbContextProvider>
+        <App />
+      </BreadcrumbContextProvider>
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
+};
+
+window.OpAuthHelper ? window.OpAuthHelper.onLogin(renderApp) : renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
