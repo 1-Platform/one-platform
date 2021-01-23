caab286a7528d4633016d9222e2290f9a6ae5434
Rigin Oommen <riginoommen@gmail.com>
2021-01-23 15:55:42 +0530

    Fixed the error handling and index documentation

--- packages/feedback-service/src/resolver.ts
+++ packages/feedback-service/src/resolver.ts
@@ -23,145 +23,149 @@ export const FeedbackResolver = {
           });
         }).then((feedback: any) => {
           feedbackList = feedback;
-          feedback = _.groupBy(feedback, 'feedback.sourceUrl');
-          Object.keys(feedback).map((key: string) => {
-            feedback[key].forEach((groupedList: FeedbackType, index: any) => {
-              timestampQuery += `
-              rhatUUID_${(groupedList.createdBy as string).replace(/-/g, '')}:getUsersBy(rhatUUID:"${groupedList.createdBy}") {
-                name
-                title
-                uid
-                rhatUUID
-              }
-              
-              ${(groupedList.updatedBy) ? `
-              rhatUUID_${(groupedList.updatedBy as string).replace(/-/g, '')}:getUsersBy(rhatUUID:"${groupedList.updatedBy}") {
-                name
-                title
-                uid
-                rhatUUID
-              }
-              `: ``}
-              `;
-              if ((groupedList as any).feedback.source === 'GITLAB') {
-                query += `
-                      gitlab_${index}_${groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 1]}:project(fullPath: "${(groupedList as any).feedback.projectKey}") {
-                        issue(iid: "${groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 1]}") {
-                        title
-                        description
-                        state
-                        webUrl
-                        labels {
-                            edges {
-                            node {
-                                title
-                            }
-                            }
-                        }
-                        assignees {
-                            nodes {
-                            name
-                            email
-                            webUrl
-                            }
-                        }
-                        }
+          if (feedbackList.length) {
+            feedback = _.groupBy(feedback, 'feedback.sourceUrl');
+            Object.keys(feedback).map((key: string) => {
+              feedback[key].forEach((groupedList: FeedbackType, index: any) => {
+                timestampQuery += `
+                rhatUUID_${(groupedList.createdBy as string).replace(/-/g, '')}:getUsersBy(rhatUUID:"${groupedList.createdBy}") {
+                  name
+                  title
+                  uid
+                  rhatUUID
+                }
+                
+                ${(groupedList.updatedBy) ? `
+                rhatUUID_${(groupedList.updatedBy as string).replace(/-/g, '')}:getUsersBy(rhatUUID:"${groupedList.updatedBy}") {
+                  name
+                  title
+                  uid
+                  rhatUUID
+                }
+                `: ``}
+                `;
+                if ((groupedList as any).feedback.source === 'GITLAB') {
+                  query += `
+                        gitlab_${index}_${groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 1]}:project(fullPath: "${(groupedList as any).feedback.projectKey}") {
+                          issue(iid: "${groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 1]}") {
+                          title
+                          description
+                          state
+                          webUrl
+                          labels {
+                              edges {
+                              node {
+                                  title
+                              }
+                              }
+                          }
+                          assignees {
+                              nodes {
+                              name
+                              email
+                              webUrl
+                              }
+                          }
+                          }
+                      }
+                      `;
+                  if (feedback[key].length === index + 1) {
+                    query = `query listGitlabIssues {${query} }`;
+                    const gitlabQuery = {
+                      'query': query,
+                      'sourceUrl': key,
+                      'source': (groupedList as any).feedback.source
                     }
-                    `;
-                if (feedback[key].length === index + 1) {
-                  query = `query listGitlabIssues {${query} }`;
-                  const gitlabQuery = {
-                    'query': query,
-                    'sourceUrl': key,
-                    'source': (groupedList as any).feedback.source
+                    query = '';
+                    queryList.push(gitlabQuery);
                   }
-                  query = '';
-                  queryList.push(gitlabQuery);
-                }
-              } else if ((groupedList as any).feedback.source === 'GITHUB') {
-                query += `
-                  gitlab_${index}_${groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 1]}:repository(name: "${groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 3]}", owner: "${groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 4]}") {
-                        issue(number: ${Number(groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 1])}) {
-                        url
-                        title
-                        body
-                        state
-                        author {
-                            login
-                            avatarUrl
-                        }
-                        assignees(first:100) {
-                            nodes {
-                            name
-                            email
-                            url
-                            }
-                        }
-                        }
-                    }`;
-                if (feedback[key].length === index + 1) {
-                  query = `query listGithubIssues {${query} }`;
-                  const githubQuery = {
-                    'query': query,
-                    'sourceUrl': key,
-                    'source': (groupedList as any).feedback.source
+                } else if ((groupedList as any).feedback.source === 'GITHUB') {
+                  query += `
+                    gitlab_${index}_${groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 1]}:repository(name: "${groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 3]}", owner: "${groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 4]}") {
+                          issue(number: ${Number(groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 1])}) {
+                          url
+                          title
+                          body
+                          state
+                          author {
+                              login
+                              avatarUrl
+                          }
+                          assignees(first:100) {
+                              nodes {
+                              name
+                              email
+                              url
+                              }
+                          }
+                          }
+                      }`;
+                  if (feedback[key].length === index + 1) {
+                    query = `query listGithubIssues {${query} }`;
+                    const githubQuery = {
+                      'query': query,
+                      'sourceUrl': key,
+                      'source': (groupedList as any).feedback.source
+                    }
+                    query = '';
+                    queryList.push(githubQuery);
                   }
-                  query = '';
-                  queryList.push(githubQuery);
-                }
-              } else if ((groupedList as any).feedback.source === 'JIRA') {
-                query += `${groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 1]} `;
-                if (feedback[key].length === index + 1) {
-                  const jiraQuery = {
-                    'query': query,
-                    'sourceUrl': key,
-                    'source': (groupedList as any).feedback.source
+                } else if ((groupedList as any).feedback.source === 'JIRA') {
+                  query += `${groupedList.ticketUrl.split('/')[groupedList.ticketUrl.split('/').length - 1]} `;
+                  if (feedback[key].length === index + 1) {
+                    const jiraQuery = {
+                      'query': query,
+                      'sourceUrl': key,
+                      'source': (groupedList as any).feedback.source
+                    }
+                    query = '';
+                    queryList.push(jiraQuery);
                   }
-                  query = '';
-                  queryList.push(jiraQuery);
                 }
-              }
+              });
             });
-          });
+          }
         }).then(async () => {
-          timestampQuery = `query ListUsers {
-            ${timestampQuery}
-          }`;
-          userData = await FeedbackIntegrationHelper.getUserProfiles(timestampQuery);
-          queryList.map(async (queries: any) => {
-            if (queries.source === 'GITLAB') {
-              const gitlabPromise = new Promise(async (resolve, reject) => {
-                const gitlabResponse: Array<object> = await FeedbackIntegrationHelper.listGitlabIssues(queries);
-                resolve(gitlabResponse);
-              }).catch((err: Error) => {
-                throw err
-              });
-              promises.push(gitlabPromise)
-            } else if (queries.source === 'GITHUB') {
-              const githubPromise = new Promise(async (resolve, reject) => {
-                const githubResponse: Array<object> = await FeedbackIntegrationHelper.listGithubIssues(queries);
-                resolve(githubResponse);
-              }).catch((err: Error) => {
-                throw err
-              });
-              promises.push(githubPromise)
-            } else if (queries.source === 'JIRA') {
-              queries.query.trim().split(' ').map((issue: any) => {
-                const jiraPromise = new Promise(async (resolve, reject) => {
-                  const query = {
-                    query: issue,
-                    source: queries.source,
-                    sourceUrl: queries.sourceUrl
-                  };
-                  const jiraResponse: any = await FeedbackIntegrationHelper.listJira(query);
-                  resolve(jiraResponse);
+          if (feedbackList.length) {
+            timestampQuery = `query ListUsers {
+              ${timestampQuery}
+            }`;
+            userData = await FeedbackIntegrationHelper.getUserProfiles(timestampQuery);
+            queryList.map(async (queries: any) => {
+              if (queries.source === 'GITLAB') {
+                const gitlabPromise = new Promise(async (resolve, reject) => {
+                  const gitlabResponse: Array<object> = await FeedbackIntegrationHelper.listGitlabIssues(queries);
+                  resolve(gitlabResponse);
                 }).catch((err: Error) => {
                   throw err
                 });
-                promises.push(jiraPromise);
-              });
-            }
-          });
+                promises.push(gitlabPromise)
+              } else if (queries.source === 'GITHUB') {
+                const githubPromise = new Promise(async (resolve, reject) => {
+                  const githubResponse: Array<object> = await FeedbackIntegrationHelper.listGithubIssues(queries);
+                  resolve(githubResponse);
+                }).catch((err: Error) => {
+                  throw err
+                });
+                promises.push(githubPromise)
+              } else if (queries.source === 'JIRA') {
+                queries.query.trim().split(' ').map((issue: any) => {
+                  const jiraPromise = new Promise(async (resolve, reject) => {
+                    const query = {
+                      query: issue,
+                      source: queries.source,
+                      sourceUrl: queries.sourceUrl
+                    };
+                    const jiraResponse: any = await FeedbackIntegrationHelper.listJira(query);
+                    resolve(jiraResponse);
+                  }).catch((err: Error) => {
+                    throw err
+                  });
+                  promises.push(jiraPromise);
+                });
+              }
+            });
+          }
         }).then(async () => {
           const responses = await Promise.all(promises).then((values) => _.flattenDeep(values));
           return feedbackList.map((feedback: FeedbackType) => {
@@ -350,6 +354,50 @@ P.S.: This is an automated email. Please do not reply.
           return response;
         })
         .catch((error: Error) => error);
+    },
+    updateFeedbackIndex(root: any, args: any, ctx: any) {
+      let userQueryParams = ``;
+      let searchInput: any = [];
+      return Feedback.find()
+        .then(async (response: any) => {
+          response.map((data: any) => {
+            userQueryParams += `rhatUUID_${(data.createdBy as string).replace(/-/g, '')}:getUsersBy(rhatUUID:"${data.createdBy}") {
+              name
+              title
+              uid
+              rhatUUID
+            }
+            ${(data.updatedBy) ? `
+            rhatUUID_${(data.updatedBy as string).replace(/-/g, '')}:getUsersBy(rhatUUID:"${data.updatedBy}") {
+              name
+              title
+              uid
+              rhatUUID
+            }`: ``}`;
+            const formattedData = FeedbackIntegrationHelper.formatSearchInput(data);
+            searchInput.push(formattedData);
+          });
+          let userQuery = `
+        query ListUsers {
+          ${userQueryParams}
+        }
+        `;
+          let userData = await FeedbackIntegrationHelper.getUserProfiles(userQuery);
+          const indexStatus = searchInput.map((searchData: any) => {
+            searchData.input.documents.createdBy = userData.filter((user: any) => user.rhatUUID === searchData.input.documents.createdBy)[0]?.name;
+            searchData.input.documents.updatedBy = userData.filter((user: any) => user.rhatUUID === searchData.input.documents?.updatedBy)[0]?.name;
+            return FeedbackIntegrationHelper.manageSearchIndex(searchData, 'index');
+          });
+
+          if (indexStatus.length) {
+            const indexResponse = {
+              status: 200
+            };
+            return indexResponse;
+          }
+
+        })
+        .catch((err: Error) => err);
     }
   }
 };
