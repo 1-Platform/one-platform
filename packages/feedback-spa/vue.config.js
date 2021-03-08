7552f8dc0684e8da8e369215f871f4d7a3166936
Rigin Oommen <riginoommen@gmail.com>
2021-03-08 13:36:47 +0530

    Updated the Icons pack

--- packages/feedback-spa/vue.config.js
+++ packages/feedback-spa/vue.config.js
@@ -2,13 +2,17 @@ module.exports = {
   publicPath: '/feedback',
   chainWebpack: config => {
     config
-      .plugin('html')
-      .tap(args => {
-        args[0].minify = {
-          ...args[0].minify,
+      .plugin( 'html' )
+      .tap( args => {
+        args[ 0 ].minify = {
+          ...args[ 0 ].minify,
           removeComments: false
-        }
-        return args
-      })
+        };
+        return args;
+      } );
+  },
+  pwa: {
+    name: 'Feedback SPA',
+    short_name: 'feedback-spa'
   }
-}
+};
