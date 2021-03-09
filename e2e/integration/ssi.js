98d146039f624fd571412873845ecebf95cae148
supriyagawade <gawadesupriya05@gmail.com>
2021-03-09 11:30:28 +0530

    added tests for ssi (#908)
    Co-authored-by: sgawade <sgawade@redhat.com>

---
+++ e2e/integration/ssi.js
@@ -0,0 +1,55 @@
+import { apps } from "../helper/apps.list";
+context( 'Test for SSI', () => {
+
+    before( () => {
+        cy.visit( Cypress.env( 'STAGE_HOST' ) );
+        Cypress.config( 'includeShadowDom', true );
+        cy.get( '#username', { timeout: 5000 } ).type( Cypress.env( 'USERNAME' ) );
+        cy.get( '#password' ).type( Cypress.env( 'PASSWORD' ) );
+        cy.get( '#submit' ).click();
+    } );
+    Cypress.on( 'uncaught:exception', ( err, runnable ) => {
+        return false;
+    } );
+
+    it( 'Test that all the deployed Apps are visible', () => {
+        cy.get( '.op-menu__item-button' ).first().click();
+        cy.get( '.apps__list' ).should( 'be.visible' ).within( () => {
+            apps.forEach( function ( item ) {
+                cy.get( `a[ href = "${ item }" ]` ).should( 'be.visible' );
+            } );
+        } );
+    } );
+
+    it( 'Test for Notifications section', () => {
+        cy.get( '.op-nav-wrapper' ).within( () => {
+            cy.contains( 'Notifications' ).click();
+        } );
+        cy.get( '#op-menu-drawer' ).should( 'be.visible' ).within( () => {
+            cy.get( '.op-menu-drawer__title' ).should( 'contain.text', 'All Notifications ' );
+        } );
+    } );
+
+    it( 'Test for User Profile Section', () => {
+        cy.get( '.op-nav-wrapper' ).within( () => {
+            cy.get( '.op-menu__item' ).last().click();
+        } );
+        cy.get( '#op-menu-drawer' ).should( 'be.visible' ).within( () => {
+            //user profile icon
+            cy.get( '.op-user-profile-icon' ).should( 'be.visible' );
+            //User profile name
+            cy.get( '.op-menu-drawer__title' ).should( 'contain.text', `${ Cypress.env( 'USERNAME' ) } sso-tester` );
+            //Signout section
+            cy.get( '.op-user-signout-btn' ).should( 'contain.text', 'Sign Out' );
+            //View profile section
+            cy.get( `a[href="/user-groups/user/${ Cypress.env( 'USERNAME' ) }"]` ).should( 'contain.text', 'View Profile' );
+
+        } );
+    } );
+
+    it( 'Test for Get started Section', () => {
+        cy.get( '.op-nav-wrapper' ).within( () => {
+            cy.get( 'a[ href = "/get-started" ]' ).should( 'be.visible' );
+        } );
+    } );
+} );
