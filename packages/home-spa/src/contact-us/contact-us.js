24dab237494bd38693f734b7dd5ad67528e41cad
akhilmhdh <akhilmhdh@gmail.com>
2021-06-30 18:19:19 +0530

    feat(1226): changed hardcoded contact us list to user-group api list

--- packages/home-spa/src/contact-us/contact-us.js
+++ packages/home-spa/src/contact-us/contact-us.js
@@ -1,61 +1,24 @@
+import { getContactUsTeamDetails, getData } from '../service/service';
+
 export const contactUsTeamBlock = () => {
-  const teamMembers = [
-    {
-      name: 'Nilesh Patil',
-      title: 'Manager, Software Engineering',
-      gender: 'male',
-    },
-    {
-      name: 'Sayak Sarkar',
-      title: 'Principle Software Engineer',
-      gender: 'male',
-    },
-    {
-       name: 'Ghanshyam Lohar',
-       title: 'Senior Software Engineer',
-       gender: 'male',
-    },
-    {
-      name: 'Diwanshi Pandey',
-      title: 'Senior Software Engineer',
-      gender: 'female',
-    },
-    {
-      name: 'Rigin Oommen',
-      title: 'Senior Software Engineer',
-      gender: 'male',
-    },
-    {
-      name: 'Mayur Deshmukh',
-      title: 'Software Engineer',
-      gender: 'male'
-    },
-    {
-      name: 'Sumeet Ingole',
-      title: 'Software Engineer',
-      gender: 'male'
-    },
-    {
-      name: 'Prasad Ambulkar',
-      title: 'Software Engineer',
-      gender: 'male'
-    },
-    {
-      name: 'Deepesh Nair',
-      title: 'Software Engineer',
-      gender: 'male'
-    },
-  ];
   const teamBlock = document.querySelector('#team-block');
-  if (teamBlock !== null) {
-    teamBlock.innerHTML = teamMembers.map(member => {
-      return `
+  if ( teamBlock !== null ) {
+    getData( getContactUsTeamDetails ).then( ( result ) => {
+        const teamMembers = result.data.group.members.sort((a, b) =>
+          a.cn.localeCompare(b.cn)
+        );
+      teamBlock.innerHTML = teamMembers
+        .map((member) => {
+          return `
       <div class="team-block__list--item">
-          <img src="/res/img/${member.gender === 'male' ? 'user-male.svg': 'user-female.svg'}" alt="">
-          <span class="name">${member.name}</span>
-          <span class="title">${member.title}</span>
+          <img src="/res/img/avatar.svg" alt="">
+          <span class="name">${member.cn}</span>
+          <span class="title">${member.rhatJobTitle}</span>
       </div>`;
+        })
+        .join("");
+    } ).catch( ( err ) => {
+      console.error( err );
     })
-    .join('');
   }
 }
