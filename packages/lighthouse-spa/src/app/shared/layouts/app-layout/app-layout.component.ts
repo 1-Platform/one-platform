937c02901070f52e7d9a006898801c635e72a880
akhilmhdh <akhilmhdh@gmail.com>
2021-07-13 12:08:34 +0530

    fix: fixed links of lighthouse-spa

--- packages/lighthouse-spa/src/app/shared/layouts/app-layout/app-layout.component.ts
+++ packages/lighthouse-spa/src/app/shared/layouts/app-layout/app-layout.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from 'environments/environment';
 @Component({
   selector: 'app-app-layout',
   templateUrl: './app-layout.component.html',
-  styleUrls: ['./app-layout.component.scss']
+  styleUrls: ['./app-layout.component.scss'],
 })
 export class AppLayoutComponent implements OnInit {
+  lighthouseServerUrl = environment.LH_SERVER_URL;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
