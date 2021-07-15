d2fa722e9e9a3035aceedeebe8c07edbeaf79e32
hybridx <hybridx18@gmail.com>
2021-07-15 16:15:13 +0530

    fix(#1024) - Added logic for SPADeployment in home-spa
    fix(#1024) - Added logic for SPADeployment in home-spa
    
    fix(#1024) - Remove extra not required code

--- packages/home-spa/src/service/service.js
+++ packages/home-spa/src/service/service.js
@@ -45,6 +45,31 @@ export function getData(query) {
     .catch( ( err ) => { console.error( err ); } );
 }
 
+export const addApp = (appData) => {
+  const fetchOptions = {
+    method: 'post',
+    headers: {
+      Authorization: `Bearer ${window.OpAuthHelper?.jwtToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: 'mutation QuickDeploy($app: CreateAppInput!){ createApp(app: $app) { id name path } }',
+      variables: {
+        app: {
+          name: appData.name,
+          path: appData.path,
+          description: appData.description,
+          isActive: true,
+          applicationType: 'HOSTED',
+        }
+      }
+    }),
+  };
+  return fetch(process.env.API_URL, fetchOptions)
+    .then( handleError )
+    .then((res) => res.json());
+};
+
 export function deploySPA(formData) {
   const fetchOptions = {
     method: 'post',
@@ -55,9 +80,6 @@ export function deploySPA(formData) {
     body: formData,
   };
   return fetch(process.env.DEPLOY_URL, fetchOptions)
-    .then(handleError)
-    .then((res) => res.json())
-    .catch((err) => {
-      console.error(err);
-    });
+    .then( handleError )
+    .then((res) => res.json());
 }
