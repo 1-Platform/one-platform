5409a697eb98a14aa22264ea8f4dd3fb0c982caa
akhilmhdh <akhilmhdh@gmail.com>
2021-08-02 14:12:14 +0530

    feat(1370): updated ui of lh with new changes

--- packages/lighthouse-spa/src/app/dashboard/dashboard-routing.module.ts
+++ packages/lighthouse-spa/src/app/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnalysisComponent } from './pages/analysis/analysis.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent,
+    component: HomeComponent,
   },
   {
     path: ':id',
--- packages/lighthouse-spa/src/app/shared/components/lhtag-line-steps/lhtag-line-steps.component.spec.ts
+++ packages/lighthouse-spa/src/app/shared/components/lhtag-line-steps/lhtag-line-steps.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DashboardComponent } from './dashboard.component';
+import { LHTagLineStepsComponent } from './lhtag-line-steps.component';
 
-describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+describe('LHTagLineStepsComponent', () => {
+  let component: LHTagLineStepsComponent;
+  let fixture: ComponentFixture<LHTagLineStepsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ LHTagLineStepsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
+    fixture = TestBed.createComponent(LHTagLineStepsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
