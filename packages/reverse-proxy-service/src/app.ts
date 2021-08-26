8c7060573b3ce8f448ca5e373366b07b10b1b372
Mayur Deshmukh <deshmukhmayur@outlook.com>
2021-08-26 13:18:12 +0530

    Adds cors middleware to the couchdb endpoint (#1086)

--- packages/reverse-proxy-service/src/app.ts
+++ packages/reverse-proxy-service/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 import jwtAuth from './middleware/jwtAuth';
 import couchDBRouter from './couchdb';
@@ -10,6 +11,6 @@ app.get( '/api', ( _, res ) => {
   res.json( { message: 'This is a proxy service.' } );
 } );
 
-app.use( '/api/couchdb', [ jwtAuth ], couchDBRouter );
+app.use( '/api/couchdb', [ cors(), jwtAuth ], couchDBRouter );
 
 export default app;
