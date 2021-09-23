d74fe90a484ecc45a3ed60a8c21456c0b37fb423
Mayur Deshmukh <deshmukhmayur@outlook.com>
2021-09-23 13:45:05 +0530

    Added winston for better logging in reverse-proxy

--- packages/reverse-proxy-service/src/index.ts
+++ packages/reverse-proxy-service/src/index.ts
@@ -1,6 +1,7 @@
 import app from './app';
 import { PORT } from './config/env';
+import logger from './utils/logger';
 
 app.listen( PORT, () => {
-  console.log( `Reverse proxy listening on port ${PORT}` );
+  logger.info( `Reverse proxy listening on port ${PORT}` );
 } );
