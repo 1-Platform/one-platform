3b9efa8b65c0ae7eebcc8e6c3abd9778d2ef81f3
hybridx <hybridx18@gmail.com>
2021-10-08 11:26:01 +0530

    feat(UI): Added patternflyElements-card
    Removed extra unused scss

--- packages/component-catalog-spa/src/assets/component-libraries.js
+++ packages/component-catalog-spa/src/assets/component-libraries.js
@@ -4,32 +4,32 @@ import cpx from './images/chapeaux-logo.svg';
 import pf from './images/patternfly-logo.svg';
 
 export const Libraries = [
-    {
-        id: 1,
-        title: 'Chapeaux',
-        logo: cpx,
-        description: 'There are different types of secrets. She had held onto plenty of them during her life, but this one was different.',
-        link: 'https://github.com/chapeaux',
-    },
-    {
-        id: 2,
-        title: 'Patternfly Elements',
-        logo: pfe,
-        description: 'There are different types of secrets. She had held onto plenty of them during her life, but this one was different.',
-        link: 'https://github.com/patternfly/patternfly-elements',
-    },
-    {
-        id: 3,
-        title: 'One Platform Components',
-        logo: op,
-        description: 'There are different types of secrets. She had held onto plenty of them during her life, but this one was different.',
-        link: 'https://github.com/1-platform/op-components',
-    },
-    {
-        id: 4,
-        title: 'PatternFly React Components',
-        logo: pf,
-        description: 'There are different types of secrets. She had held onto plenty of them during her life, but this one was different.',
-        link: 'https://github.com/patternfly/patternfly-react',
-    },
+  {
+    id: 1,
+    title: 'Chapeaux',
+    logo: cpx,
+    description: 'A relatively unopinionated toolset built around enterprise-grade, functional, web components.',
+    link: 'https://github.com/chapeaux',
+  },
+  {
+    id: 2,
+    title: 'Patternfly Elements',
+    logo: pfe,
+    description: 'A set of community-created web components based on PatternFly design.',
+    link: 'https://github.com/patternfly/patternfly-elements',
+  },
+  {
+    id: 3,
+    title: 'One Platform Components',
+    logo: op,
+    description: 'One platform component library is a collection of web components which are built using lit-element, each component follows the red hat brand guideline.',
+    link: 'https://github.com/1-platform/op-components',
+  },
+  {
+    id: 4,
+    title: 'PatternFly React Components',
+    logo: pf,
+    description: 'PatternFly is an open source design system created to enable consistency and usability across a wide range of applications and use cases. PatternFly provides clear standards, guidance, and tools that help designers and developers work together more efficiently and build better user experiences.',
+    link: 'https://github.com/patternfly/patternfly-react',
+  },
 ];
--- packages/component-catalog-spa/src/components/Card/Card.jsx
+++ packages/component-catalog-spa/src/components/Card/Card.jsx
@@ -1,43 +1,27 @@
 import './Card.scss'
-import githubIcon from '../../assets/images/github-icon.svg';
 import { APP_THEMES } from '../../constants';
 import opLogo from '../../assets/images/op-logo.svg';
+import '@patternfly/pfe-card';
+import '@patternfly/pfe-cta';
 
-/**
- * 
- * @param {theme} theme Theme of the component (default=dark)
- * @param {fragment} fragment Rest of the class
- * @returns Generated class name e.g. card-dark__content
- */
-const generateClass = (theme=APP_THEMES.dark, fragment = '') => {
-  const cardThemes = {
-    light: 'card-light',
-    dark: 'card-dark'
-  }
-  return cardThemes[theme] + fragment;
-}
-
-const Card = ({theme, logo, name, description, link }) => {
+const Card = ({logo, name, description, link }) => {
   return (
-    <div className={generateClass(theme)}>
-      <div className={generateClass(theme, '__content')}>
-        <div className={generateClass(theme, '__logo')}>
-          <img src={logo} alt="logo" />
-        </div>
-        <h3 className={generateClass(theme, '__content-header')}>
-          {name}
-        </h3>
-        <p className={generateClass(theme, '__content-information')}>
-          {description}
-        </p>
-        <div className={generateClass(theme, '__footer')}>
-          <a target="_blank" rel="noreferrer" href={link}>
-            <img src={githubIcon} alt="otto-cat" /> 
-            <label> Github </label>
-          </a>
-        </div>
+  <>
+    <pfe-card color="lightest" border>
+      <div>
+        <img src={logo} alt="logo" />
       </div>
-    </div>
+      <h3>
+        {name}
+      </h3>
+      <p>
+        {description}
+      </p>
+      <div slot="pfe-card--footer">
+        <pfe-cta><a target="_blank" rel="noreferrer" href={link}>View on Github</a></pfe-cta>
+      </div>
+    </pfe-card>
+    </>
   );
 }
 
