aab3f4288fbd9bd2ec0376ab87a18e0541dd63ed
Deepesh Nair <hybridx18@gmail.com>
2021-10-11 12:12:27 +0530

    chore1201) - Alignment fix for cards (#1220)
    * ui(1201) - Fixed alignment
    
    * ui(1201) - Removed patternfly element card and added React Card
    
    * ui(1201) - Fixed code style in Main.jsx

--- packages/component-catalog-spa/src/pages/Main.jsx
+++ packages/component-catalog-spa/src/pages/Main.jsx
@@ -1,13 +1,55 @@
 import './Main.scss';
-import Card from '../components/Card/Card';
+import { 
+  Brand, 
+  Card, 
+  CardBody, 
+  CardFooter, 
+  CardHeader, 
+  Title, 
+  Gallery, 
+  GalleryItem, 
+  Flex, 
+  FlexItem,
+} from '@patternfly/react-core';
 import { Libraries } from '../assets/component-libraries';
 
 const Main = () => {
     return (
-        <div className="Main">
+        <Gallery className="main"
+        hasGutter
+        minWidths={{
+          default: '100%',
+          sm: '50%',
+          md: '33%',
+          lg: '25%',
+        }}>
         { Libraries.map(
-          ( lib ) => <Card theme="light" link={ lib.link } key={ lib.id } name={ lib.title } logo={ lib.logo } description={ lib.description } /> ) }
-        </div>
+          ( lib ) => 
+          <GalleryItem>
+            <Card key={ lib.id } isFullHeight isRounded>
+            <CardHeader>
+              <Flex 
+              hasGutter
+              alignItems={{ default: 'alignItemsCenter' }}>
+                <FlexItem>
+                    <Brand src={lib.logo} alt="logo" style={{ height: '48px' }} />
+                </FlexItem>
+                <FlexItem isFilled>
+                  <Title className="pf-u-ml-md" headingLevel="h2">{lib.title}</Title>
+                </FlexItem>
+              </Flex>
+            </CardHeader>
+            <CardBody isFilled>{lib.description}</CardBody>
+            <CardFooter>
+              <a target="_blank" rel="noreferrer" href={lib.link}>
+                View on Github
+                <ion-icon style={{marginBottom: '-2px', marginLeft: '.25rem'}} name="arrow-forward-outline"></ion-icon>
+              </a>
+            </CardFooter>
+          </Card> 
+        </GalleryItem>
+        ) }
+        </Gallery>
     );
 }
 
