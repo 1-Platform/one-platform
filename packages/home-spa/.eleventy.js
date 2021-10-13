b1fabee4cc63bed91a42bee07b72512b471bd15e
hybridx <hybridx18@gmail.com>
2021-10-13 12:05:45 +0530

    chore(#1115) - Added new home-spa

---
+++ packages/home-spa/.eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('./src/*.css');
+  eleventyConfig.addPassthroughCopy('./src/static');
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    }
+  }
+}
