2849acc5b1d55d67e879ec953d1ff400d7459951
hybridx <hybridx18@gmail.com>
2021-10-14 12:14:39 +0530

    chore(#1115) - Removed extra variable SPAShip link

--- packages/home-spa/src/_data/env.js
+++ packages/home-spa/src/_data/env.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv-safe');
 dotenv.config();
 
 module.exports.API_URL = process.env.API_URL;
-module.exports.SPASHIP_API_URL = process.env.SPASHIP_API_URL;
 module.exports.KEYCLOAK_IDP_URL = process.env.KEYCLOAK_IDP_URL;
 module.exports.KEYCLOAK_CLIENT_ID = process.env.KEYCLOAK_CLIENT_ID;
 module.exports.KEYCLOAK_REALM = process.env.KEYCLOAK_REALM;
