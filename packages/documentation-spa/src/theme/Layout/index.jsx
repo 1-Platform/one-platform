ade963a363bd6bcee274505148468bb8ef90347c
Mayur Deshmukh <deshmukhmayur@outlook.com>
2021-10-20 20:10:46 +0530

    fix(docs): Rollback docusaurus version
    To fix the AnnouncementBar breaking change in the newer beta version

--- packages/documentation-spa/src/theme/Layout/index.jsx
+++ packages/documentation-spa/src/theme/Layout/index.jsx
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { ReactNode } from "react";
+import React from "react";
 import Head from "@docusaurus/Head";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
@@ -13,7 +13,6 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 import ThemeProvider from "@theme/ThemeProvider";
 import UserPreferencesProvider from "@theme/UserPreferencesProvider";
 import AnnouncementBar from "@theme/AnnouncementBar";
-import Navbar from "@theme/Navbar";
 import Footer from "@theme/Footer";
 
 import "./styles.css";
@@ -50,36 +49,38 @@ function Layout(props) {
   const faviconUrl = useBaseUrl(favicon);
 
   return (
-    <Providers>
-      <Head>
-        {/* TODO: Do not assume that it is in english language */}
-        <html lang="en" />
+    <>
+      <Providers>
+        <Head>
+          {/* TODO: Do not assume that it is in english language */}
+          <html lang="en" />
 
-        {metaTitle && <title>{metaTitle}</title>}
-        {metaTitle && <meta property="og:title" content={metaTitle} />}
-        {favicon && <link rel="shortcut icon" href={faviconUrl} />}
-        {description && <meta name="description" content={description} />}
-        {description && (
-          <meta property="og:description" content={description} />
-        )}
-        {version && <meta name="docsearch:version" content={version} />}
-        {keywords && keywords.length && (
-          <meta name="keywords" content={keywords.join(",")} />
-        )}
-        {metaImage && <meta property="og:image" content={metaImageUrl} />}
-        {metaImage && <meta property="twitter:image" content={metaImageUrl} />}
-        {metaImage && (
-          <meta name="twitter:image:alt" content={`Image for ${metaTitle}`} />
-        )}
-        {permalink && <meta property="og:url" content={siteUrl + permalink} />}
-        {permalink && <link rel="canonical" href={siteUrl + permalink} />}
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <AnnouncementBar />
-      <div dangerouslySetInnerHTML={ { __html: '<!--#include virtual="/.ssi/nav/default.html" -->' } } />
-      <div className="main-wrapper">{ children }</div>
-      {!noFooter && <Footer />}
-    </Providers>
+          { metaTitle && <title>{ metaTitle }</title> }
+          { metaTitle && <meta property="og:title" content={ metaTitle } /> }
+          { favicon && <link rel="shortcut icon" href={ faviconUrl } /> }
+          { description && <meta name="description" content={ description } /> }
+          { description && (
+            <meta property="og:description" content={ description } />
+          ) }
+          { version && <meta name="docsearch:version" content={ version } /> }
+          { keywords && keywords.length && (
+            <meta name="keywords" content={ keywords.join( "," ) } />
+          ) }
+          { metaImage && <meta property="og:image" content={ metaImageUrl } /> }
+          { metaImage && <meta property="twitter:image" content={ metaImageUrl } /> }
+          { metaImage && (
+            <meta name="twitter:image:alt" content={ `Image for ${ metaTitle }` } />
+          ) }
+          { permalink && <meta property="og:url" content={ siteUrl + permalink } /> }
+          { permalink && <link rel="canonical" href={ siteUrl + permalink } /> }
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        <AnnouncementBar />
+        <div dangerouslySetInnerHTML={ { __html: '<!--#include virtual="/.ssi/nav/default.html" -->' } } />
+        <div className="main-wrapper">{ children }</div>
+        { !noFooter && <Footer /> }
+      </Providers>
+    </>
   );
 }
 
