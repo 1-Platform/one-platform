6733e8cb976c5585c9bc01b4ec1789cc9d4e2472
Akhil Mohan <akhilmhdh@gmail.com>
2021-10-21 11:32:25 +0530

    hotfix: migrated to latest opc-base and opc-drawer components (#1255)

--- packages/lighthouse-spa/src/environments/environment.ts
+++ packages/lighthouse-spa/src/environments/environment.ts
@@ -1,8 +1,12 @@
-
 export const environment = {
-   production: false,
-   API_URL: "",
-   WS_URL: "",
-   LH_SERVER_URL: "",
-   LH_CONTACT_MAIL:""
+  production: false,
+  API_URL: '',
+  WS_URL: '',
+  LH_SERVER_URL: '',
+  LH_CONTACT_MAIL: '',
+  OPCBASE_API_BASE_PATH: '',
+  OPCBASE_SUBSCRIPTION_BASE_PATH: '',
+  OPCBASE_KEYCLOAK_URL: '',
+  OPCBASE_KEYCLOAK_CLIENT_ID: '',
+  OPCBASE_KEYCLOAK_REALM: '',
 };
