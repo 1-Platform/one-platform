9c55662f1852fe29b43f769bd40ea0370c1999c6
Rigin Oommen <riginoommen@gmail.com>
2021-11-08 17:57:20 +0530

    Included the Lint Configurations

---
+++ packages/user-group-service/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "airbnb-typescript/base"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
