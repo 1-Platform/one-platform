33195be0037a9b9646ee95179b014564fbbf99ad
hybridx <hybridx18@gmail.com>
2021-11-11 16:01:48 +0530

    refactor(chore(#1294)): Moved out the notification-spa as it is depricated
    none
    
    BREAKING CHANGE: This removes the notification-spa completely from the repi

--- packages/notification-spa/src/app/helper.ts
+++
@@ -1,1 +0,0 @@
-export const UserProfile = window.OpAuthHelper?.getUserInfo();
--- packages/notification-spa/src/app/mocks/user.mock.ts
+++
@@ -1,8 +0,0 @@
-export const UserProfile = {
-    fullName: 'Deepesh Nair',
-    email: 'denair@redhat.com',
-    firstName: 'Deepesh',
-    lastName: 'Nair',
-    rhatUUID: 'a12b34c56d78e90f',
-    kerberosID: 'denair',
-};
