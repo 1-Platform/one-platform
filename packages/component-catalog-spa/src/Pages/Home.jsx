b47cde30f69f121afcc319cc20b0438df5a4ff95
hybridx <hybridx@gmail.com>
2021-12-09 14:50:41 +0530

    chore(#1306) - Added font-weight for the heading

--- packages/component-catalog-spa/src/Pages/Home.jsx
+++ packages/component-catalog-spa/src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import Main from '../Components/Main';
 const Home = () => {
   return <>
   <main>
-    <h3>Components Catalog</h3>
+    <h3 className="pf-c-title pf-m-3xl" style={ {fontWeight: 300}}>Components Catalog</h3>
     <p>
       A Unified interface to access components from Chapeaux, Patternfly
       elements and One Platform components, which allows in increase of
