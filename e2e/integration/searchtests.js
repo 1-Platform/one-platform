375f3e7a55270e420d3fe4524c844847d02d2614
mudit94 <mudit94@gmail.com>
2021-12-13 12:00:05 +0530

    modifying searchtests,feedbacktests and apicatalogtests

--- e2e/integration/searchtests.js
+++ e2e/integration/searchtests.js
@@ -1,7 +1,8 @@
 /// <reference types="Cypress" />
+
 context( 'Test search', () => {
     before( () => {
-        cy.visit( Cypress.env( 'QA_HOST' ) );
+        cy.visit( Cypress.env( 'QA_HOST' )+'/search' );
         cy.get( '#username', { timeout: 5000 } ).type( Cypress.env( 'USERNAME' ) );
         cy.get( '#password' ).type( Cypress.env( 'PASSWORD' ) );
         cy.get( '#submit' ).click();
@@ -9,14 +10,14 @@ context( 'Test search', () => {
     Cypress.on( 'uncaught:exception', ( err, runnable ) => {
         return false;
     } );
-
     it( 'Test for valid search count', () => {
+        cy.wait( 1500 )
         cy.get( '.opc-nav-search__btn' ).click( { force: true } )
-        cy.wait(2000)
-        cy.get( 'input[name="query"]' ).type( 'Feedback', { force: true } );
         cy.wait(500)
-        cy.get( '.opc-nav-search__btn' ).click();
-        cy.get( '.pf-u-mt-xl' ).should( 'include.text','results found')
+        cy.get('input[name="query"]').click( { force: true } ).clear( { force: true } ).type( 'Feedback', { force: true } );
+        cy.wait(500)
+        cy.get( '.opc-nav-search__btn', { timeout: 6000 } ).click( { force: true } );
+        cy.contains('results found' ).should( 'be.visible')
     })
 
     it( 'Test for search result', () => {
@@ -72,7 +73,7 @@ context( 'Test search', () => {
         cy.get( '.search-result-section', { timeout: 5000 } ).should( 'be.visible' ).within( () => {
             cy.get( '.pf-u-mt-md' ).should( 'be.visible' ).each( (elem,index) => {
                 cy.get( '.search-tag' ).each( ( elem2, index ) => {
-                    cy.wrap( elem2 ).should( 'be.visible' ).should( 'contain.text', 'Feedback, BUG' );
+                    cy.wrap( elem2 ).should( 'be.visible' ).should( 'contain.text', 'Feedback' );
                 })
             } );
         } )
@@ -80,14 +81,13 @@ context( 'Test search', () => {
     } )
 
     it( 'Test for invalid search', () => {
-        cy.get( 'input[name="query"]' ).click( { force: true } ).clear( { force: true } ).type( 'qwertyuiopasdgthchgbtnjkmnvxs' ,{force:true});
-        cy.get( '.op-search__btn' ).click();
-        cy.get( '#username', { timeout: 5000 } ).type( Cypress.env( 'USERNAME' ) );
-        cy.get( '#password' ).type( Cypress.env( 'PASSWORD' ) );
-        cy.get( '#submit' ).click();
+        cy.wait( 1500 );
+        cy.get( '.opc-nav-search__btn' ).click( { force: true } );
+        cy.wait( 500 );
+        cy.get( 'input[name="query"]' ).click( { force: true } ).clear( { force: true } ).type( 'qwertyuiopasdgthchgbtnjkmnvxs', { force: true } );
+        cy.get( '.opc-nav-search__btn' ).click( { force: true } );
         cy.get( '.search-result-section', { timeout: 5000 } ).should( 'be.visible' ).within( () => {
             cy.contains( "Can't find anything related to ", { timeout: 5000 }).should( 'be.visible' );
             } );
     } )
-
 })
