6ccd08b6f51e43048e15bc36da3aa4daee1cc788
mudit94 <mudit94@gmail.com>
2021-12-13 14:38:31 +0530

    added search.edit and delete functionality for graphql api catalog

--- e2e/integration/apicatalogtests.js
+++ e2e/integration/apicatalogtests.js
@@ -19,7 +19,7 @@ context( 'API Catalog Tests', () => {
     } )
     it( 'Fill the add API form', () => {
         cy.get( 'input[name="name"]' ).type( 'e2e test automation' );
-        cy.get( 'textarea[name="description"]' ).type( "test" )
+        cy.get( 'textarea[name="description"]' ).type( "test is on for the api catalog. it seems to be an interesting application. i am liking it very much . this is a test by one of the tester . it seems there are very less bugs in this application. there are more of user experience difficulties found in this spa." )
         cy.contains( "REST" ).click( { force: true } )
         cy.get( '#app-layout-page > section.pf-c-page__main-section.pf-m-no-padding.pf-m-light.appLayout_app-layout--content__1Gx8I > section > div > div > div > div:nth-child(2) > form > div:nth-child(4) > div.pf-c-form__group-control > div' ).within( () => {
             cy.get('input[placeholder ^= "Search" ]' ).type( 'portal-test' ).then( () => {
@@ -92,17 +92,99 @@ context( 'API Catalog Tests', () => {
     it( 'Verifying edit API functionality', () => {
         cy.contains( 'Edit' ).click( { force: true } )
         cy.get( 'input[name="name"]' ).clear().type( 'e2e test automation update' );
-        cy.get( '.pf-c-button.pf-m-primary.pf-m-progress' ).click( { force: true } )
-
+        cy.get( '.pf-c-button.pf-m-primary.pf-m-progress' ).click( { force: true } );
+        cy.get( 'h1' ).should( 'have.text', 'e2e test automation update' );
 
     } )
     it( 'Verify Delete API', () => {
         cy.contains( 'Edit' ).click( { force: true } )
         cy.contains( 'Delete' ).click( { force: true } )
-        cy.get("footer[class='pf-c-modal-box__footer'] button:nth-child(1)").click({force:true})
+        cy.get( "footer[class='pf-c-modal-box__footer'] button:nth-child(1)" ).click( { force: true } )
+        cy.wait(2000)
     })
     it( 'Click on Explore APIs', () => {
-        cy.contains('Explore').click({force:true})
+        cy.contains( 'Explore' ).click( { force: true } )
+        cy.wait(4000)
+        cy.get( '.pf-m-fill > a > .pf-c-button' ).click( { force: true } )
+
+    } )
+    it( 'Add GraphQL API Test', () => {
+        cy.wait(8000)
+        cy.get( 'input[name="name"]' ).type( 'e2e test graphql automation' );
+        cy.get( 'textarea[name="description"]' ).type( "test is on for the api catalog. it seems to be an interesting application. i am liking it very much . this is a test by one of the tester . it seems there are very less bugs in this application. there are more of user experience difficulties found in this spa." )
+        cy.contains( "GraphQL" ).click( { force: true } )
+        cy.get( '#app-layout-page > section.pf-c-page__main-section.pf-m-no-padding.pf-m-light.appLayout_app-layout--content__1Gx8I > section > div > div > div > div:nth-child(2) > form > div:nth-child(4) > div.pf-c-form__group-control > div' ).within( () => {
+            cy.get('input[placeholder ^= "Search" ]' ).type( 'portal-test' ).then( () => {
+            cy.wait( 5000 );
+            cy.contains( 'sso', { timeout: 10000 } ).should( 'not.be.disabled' ).click( { force: true } );
+            cy.get( 'span.pf-c-chip__text' ).should( 'be.visible' )
+        })
+        })
+        cy.get( '[name="appUrl"]' ).type( "https://api.gdc.cancer.gov/v0/graphql" )
+        cy.get( '[name="schemaEndpoint"]' ).type( "https://api.gdc.cancer.gov/v0/graphql" )
+        cy.get( '[aria-label^="Select a"]' ).click( { force: true } ).then( () => {
+            cy.get( 'button[role="option"]' ).then( ( elements ) => {
+                // Get a random number using the max number of elements.
+                const max = elements.length;
+                const min = 1;
+                const randomNumber = Math.floor( Math.random() * ( max - min ) + min );
+
+                // Wrap the DOM element so we can execute new cypress commands.
+                // Use the randomized number to select a DOM element, then click it.
+                cy.wrap( elements[ randomNumber ] ).click( { force: true } );
+            } );
+        })
+        cy.get( '[name="environments.0.apiBasePath"]' ).type( "https://api.gdc.cancer.gov/v0/graphql" )
+        cy.get( '.pf-c-button.pf-m-primary.pf-m-progress' ).click( { force: true } )
+        cy.wait(3000)
+
+    } )
+    it( "Subscribe GraphQL Test", () => {
+        cy.contains( 'Subscribe' ).click( { force: true } )
+        cy.wait(2000)
+        cy.contains('Subscribed to').should('be.visible')
+    } )
+    it( "Unsubscribe GraphQL Test", () => {
+        cy.wait(6000)
+        cy.contains( "Subscribed" ).click( { force: true } )
+        cy.wait( 2000 )
+        cy.contains("Unsubscribed to").should('be.visible')
+    } )
+    it( 'Click and Open Voyager', () => {
+        cy.contains( 'Voyager' ).click( { force: true } );
+        cy.wait( 6000 )
+        cy.get('.graphql-voyager').should('be.visible')
+    } )
+    it( 'Going back', () => {
+        cy.go('back')
+    } )
+    it( 'Click and Open GraphQL Playground', () => {
+        cy.contains( 'GraphQL Playground' ).click( { force: true } )
+        cy.wait( 10000 );
+        cy.get( '.sc-fONwsr.jNppkf.playground' ).should( 'be.visible' )
+        cy.go('back')
+    } )
+     it( 'Search for GraphQL API', () => {
+        cy.contains( 'My API' ).click( { force: true } );
+        cy.wait( 4000 );
+        cy.get( 'input[placeholder^="Search for APIs"]' ).type( 'e2e test graphql automation', { force: true } )
+        cy.wait( 300 )
+         cy.get('.pf-l-split__item.pf-m-fill').contains( 'e2e test graphql' ).click( { force: true } )
+         cy.wait(3000)
+    } )
+
+ it( 'Verifying edit graphql API functionality', () => {
+        cy.contains( 'Edit' ).click( { force: true } )
+        cy.get( 'input[name="name"]' ).clear().type( 'e2e test graphql automation update' );
+        cy.get( '.pf-c-button.pf-m-primary.pf-m-progress' ).click( { force: true } )
+        cy.wait( 2000 )
+        cy.get('h1').should('have.text','e2e test graphql automation update')
+
+    } )
+     it( 'Verify Delete Graphql API', () => {
+        cy.contains( 'Edit' ).click( { force: true } )
+        cy.contains( 'Delete' ).click( { force: true } )
+        cy.get("footer[class='pf-c-modal-box__footer'] button:nth-child(1)").click({force:true})
     })
 
     } );
