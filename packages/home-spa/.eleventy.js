bd96894862b56b9988899bc26955f90c43d61980
Deepesh Nair <hybridx18@gmail.com>
2021-12-24 11:46:11 +0530

    fix(#1352) -  Favicon missing and the designation of employees needs revamp and the oneplatform link is not there (#1353)
    * fix(#1352) - Added favicon and fixed avtar for contact-us also added footer links
    
    * fix(#1352) - Version bump
    
    Co-authored-by: hybridx <hybridx@gmail.com>

--- packages/home-spa/.eleventy.js
+++ packages/home-spa/.eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/*.css');
   eleventyConfig.addPassthroughCopy('./src/static');
+  eleventyConfig.addPassthroughCopy('./src/favicon.ico');
   return {
     dir: {
       input: "src",
