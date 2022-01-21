6ecc4c5cd46a9fa6db3b7e1465f3d0253fe4443e
mudit94 <mudit94@gmail.com>
2022-01-21 16:14:54 +0530

    e2e(cypressfolder): added xpath support

---
+++ cypress/support/commands.js
@@ -0,0 +1,30 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", (username, password) => {
+    cy.get('#username', { timeout: 5000 }).type(username);
+    cy.get('#password').type(password,{log:false});
+    cy.get('#submit').click();
+})
