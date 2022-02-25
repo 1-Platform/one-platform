d4181cee7b639c314c7d7a6b34a04ff57ff14ceb
mudit94 <mudit94@gmail.com>
2022-02-25 12:26:52 +0530

    test(e2e): test with new plugin config for fixing summary

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,6 +15,16 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
-  require('cypress-mochawesome-reporter/plugin')(on);
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+
+module.exports = (on) => {
+  on('before:run', async (details) => {
+    console.log('override before:run');
+    await beforeRunHook(details);
+  });
+
+  on('after:run', async () => {
+    console.log('override after:run');
+    await afterRunHook();
+  });
 };
