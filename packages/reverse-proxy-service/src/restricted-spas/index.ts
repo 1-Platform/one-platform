5f2fe6966d32eeedcefbeafe64843f3ac776dd7d
Rigin Oommen <riginoommen@gmail.com>
2022-03-22 16:43:11 +0530

    1925 - Updated the path of rhel developers guide app (#1487)
    Co-authored-by: Rigin Oommen <roommen@redhat.com>

--- packages/reverse-proxy-service/src/restricted-spas/index.ts
+++ packages/reverse-proxy-service/src/restricted-spas/index.ts
@@ -3,5 +3,6 @@ import resolver from './resolver';
 
 const router = Router();
 router.get( '/rhel-developer-guide', resolver );
+router.get( '/rhel-development-guide', resolver);
 
 export default router;
