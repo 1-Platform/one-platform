f4e6441b3c6a2c985990b597c4917c253e54256a
Deepesh Nair <hybridx18@gmail.com>
2022-03-22 15:52:30 +0530

    Chore/#1485 - Remove unused generator-op-microservice and other clean-up from the github repo (#1486)
    Co-authored-by: hybridx <hybridx@gmail.com>

--- cli-init.sh
+++
@@ -1,5 +0,0 @@
-# /bin/bash
-cd packages
-
-# Initate template QA
-../cli/generator-op-microservice/node_modules/.bin/yo --no-insight --no-update-notifier ../cli/generator-op-microservice/generators/microservice
