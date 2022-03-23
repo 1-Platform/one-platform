dd4a4bae48c40577b7a6c3b25f31c02d208829e7
hybridx <hybridx@gmail.com>
2022-03-23 16:15:50 +0530

    fix(1491) - Added copy of .htaccess in 11ty config

--- packages/home-spa/.eleventy.js
+++ packages/home-spa/.eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/*.css');
   eleventyConfig.addPassthroughCopy('./src/static');
   eleventyConfig.addPassthroughCopy('./src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('./.htaccess');
   return {
     dir: {
       input: "src",
