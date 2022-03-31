55705729fa3d5efabfc7a625cd458eaf127afae0
akhilmhdh <akhilmhdh@gmail.com>
2022-03-31 13:37:58 +0530

    fix(api-catalog-spa): homepage url makes screen blank

--- packages/api-catalog-spa/src/router/Router.tsx
+++ packages/api-catalog-spa/src/router/Router.tsx
@@ -4,5 +4,5 @@ import { routes } from './routes';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const Router = (): ReactElement<any, string | JSXElementConstructor<any>> | null => {
-  return useRoutes(routes, process.env.PUBLIC_URL);
+  return useRoutes(routes);
 };
