645fc72b2146fa7ced10ad2847167f66bcf86c79
Mayur Deshmukh <deshmukhmayur@outlook.com>
2022-03-31 19:05:53 +0530

    feat(rproxy): redirect for rhel-developer-guide

--- packages/reverse-proxy-service/src/restricted-spas/index.ts
+++ packages/reverse-proxy-service/src/restricted-spas/index.ts
@@ -3,7 +3,9 @@ import resolver from './resolver';
 
 const router = Router();
 
-router.get( '/rhel-developer-guide', resolver );
+router.get( '/rhel-developer-guide', ( req, res ) => {
+  res.redirect( '/rhel-development-guide', 301 );
+} );
 router.get( '/rhel-development-guide', resolver);
 router.get( '*', resolver );
 
