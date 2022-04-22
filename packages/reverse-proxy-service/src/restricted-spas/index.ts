2a047dc6702925bafaced5423900b871cfb68097
Mayur Deshmukh <deshmukhmayur@outlook.com>
2022-04-22 06:18:31 +0530

    chore(rproxy): remove redundant route resolvers (#1535)
    And updated the tests

--- packages/reverse-proxy-service/src/restricted-spas/index.ts
+++ packages/reverse-proxy-service/src/restricted-spas/index.ts
@@ -3,10 +3,11 @@ import resolver from './resolver';
 
 const router = Router();
 
+/* RHEL Development Guide permanent redirect */
 router.get( '/rhel-developer-guide', ( req, res ) => {
   res.redirect( '/rhel-development-guide', 301 );
 } );
-router.get( '/rhel-development-guide', resolver);
+/* Proxy all other apps using resolver */
 router.get( '*', resolver );
 
 export default router;
