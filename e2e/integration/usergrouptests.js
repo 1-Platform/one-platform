d3bb4d149fde1cc912d81576b9f75557d4c8fab2
Mudit Maheshwari <mudit94@gmail.com>
2022-04-22 12:29:22 +0530

    test(e2e): User group SPA additional test cases(invalid search,notification message, breadcrumb,documentation link redirection) (#1534)
    * test(e2e): user group spa (invalid search, notification messages visibility tests)
    
    * test(e2e):updated package.json version
    
    * test(e2e): changed search text to be dynamic

--- e2e/integration/usergrouptests.js
+++ e2e/integration/usergrouptests.js
@@ -1,4 +1,7 @@
 /// <reference types="Cypress" />
+
+import { lorem } from 'faker';
+
 context( 'Test for UserGroup', () => {
 
     before( () => {
@@ -21,15 +24,34 @@ context( 'Test for UserGroup', () => {
                 cy.get( '.opc-header__bottom-row' ).should( 'be.visible' );
             } );
         } )
+    } )
+    it( 'Get rover group name', () => {
+        cy.get( 'tr:nth-child(1) td:nth-child(2)' ).invoke( 'text' ).as( 'search' );
+        cy.get('tr:nth-child(1) td:nth-child(1)' ).invoke( 'text' ).as( 'searchname' );
     })
 
-    it( 'Test for search user group by name', () => {
-        cy.get( '#searchText' ).type( 'dsal-admin' )
+    it( 'Test for search user group by rover group', function()  {
+        cy.get( '#searchText' ).type( this.search );
         cy.get( 'tbody' ).within( () => {
-            cy.get( '[data-label^="LDAP"]' ).first().should( 'have.text','dsal-admin')
-        })
+            cy.get( '[data-label^="LDAP"]' ).first().should( 'have.text', this.search );
+        } )
+        cy.get( '.pf-c-search-input__text-input' ).clear();
     } )
 
+    it( 'Test for invalid search', () => {
+        cy.get( '#searchText' ).type( 'dsadsadsadsadsas' )
+        cy.contains( 'No Groups Found' ).should( 'be.visible' );
+    } )
+    it( 'Test Clear Search Visibility and Functionality', () => {
+        cy.contains( 'Clear Search' ).should( 'be.visible' ).click( { force: true } );
+        cy.get( '#searchText' ).should( 'have.text', '' );
+    })
+it( 'Test for search user group by name',function ()  {
+        cy.get( '#searchText' ).type( this.searchname )
+        cy.get( 'tbody' ).within( () => {
+            cy.get( '[data-label^="Group Name"]' ).first().should( 'have.text',this.searchname)
+        })
+    } )
     it( 'Test for user group table record display count as per filter', () => {
             cy.get( '.pf-c-options-menu__toggle-button-icon' ).click( { force: true } )
         cy.get( "ul[role='menu'] li:nth-child(1)" ).click( { force: true } )
@@ -56,39 +78,59 @@ context( 'Test for UserGroup', () => {
        })
     } )
 
-    it( 'Test for add new group', () => {
-        cy.get( 'a[href="/user-groups/group/new"]' ).click()
+    it( 'Test to check Create Group Button enablement', () => {
+        cy.contains( 'Add New Group' ).click( { force: true } );
+        cy.contains('Group Name').should('be.visible')
         cy.get( '[data-ouia-component-type="PF4/Button"]' ).should( 'be.disabled' )
         cy.get( '#groupName' ).type( 'test automation' )
+        cy.contains( 'Select Rover Group' ).should( 'be.visible' );
         cy.get( '[aria-label="Search input"]' ).type( 'test' )
         cy.get( '[data-ouia-component-type="PF4/Button"]' ).should( 'not.be.disabled' )
     } )
 
+    it( 'Test to check cancel button visibility', () => {
+           cy.contains('Cancel').should('be.visible')
+    } )
+
+    it( 'Test to check cancel button redirection', () => {
+        cy.contains( 'Cancel' ).click( { force: true } );
+        cy.get( 'table' ).should( 'be.visible' );
+    } )
+    it( 'Creating new Group Test', () => {
+        cy.contains('Add New Group').click()
+        const words = lorem.words();
+        cy.get( '#groupName' ).type( words, { force: true } );
+        cy.get( '[aria-label="Search input"]' ).type( words, { force: true } );
+         cy.get( '[aria-label="Search input"]' ).invoke('val').as('ldapname')
+        cy.xpath( '//button[normalize-space()="Add Group"]' ).click( { force: true } );
+    } )
+    it( 'Check whether Group created message is visible', () => {
+        cy.contains('Group created successfully!').should('be.visible')
+    } )
+    it( 'Test whether breadcrumb contains the ldap rover name', function() {
+        cy.get( '.pf-c-breadcrumb__list' ).should( 'contain.text', this.ldapname );
+         cy.get( '.pf-c-description-list__group' ).first().within( () => {
+             cy.get( '.pf-c-description-list__term' ).should( 'have.text', 'LDAP/Rover Common Name' );
+                cy.get( '.pf-c-description-list__description' ).should( 'have.text', this.ldapname);
+            } );
+    })
+    it( 'Edit User Profile Group Test', () => {
+        cy.xpath( '//a[normalize-space()="Edit Group"]' ).should( 'be.visible' ).click( { force: true } );
+        cy.wait( 2000 );
+        cy.contains( 'Update Group' ).click( { force: true } )
+    } )
+
+    it( 'Check whether Group Updated message is visible', () => {
+        cy.contains('Group updated successfully!').should('be.visible')
+    })
     it( 'Test for trying to add new group with existing name', () => {
-        cy.get( '.pf-c-breadcrumb__item  a[href="/user-groups"]' ).click()
-        cy.get( '#username' ).type( Cypress.env( 'USERNAME' ) );
-        cy.get( '#password' ).type( Cypress.env( 'PASSWORD' ) );
-        cy.get( '#submit' ).click();
-        cy.get( 'a[href="/user-groups/group/new"]' ).click();
+        cy.xpath('//a[normalize-space()="User Groups"]').click({force:true})
+       cy.contains( 'Add New Group' ).click( { force: true } );
         cy.get( '#groupName' ).type( 'test1' );
         cy.get( '[aria-label="Search input"]' ).type( 'one - portal - devel' );
         cy.xpath( '//button[normalize-space()="Add Group"]' ).click({force:true});
         cy.contains( 'Could not create the group').should('be.visible')
     } )
-
-    it( 'Edit a User Profile Group Test', () => {
-        cy.get( '.pf-c-breadcrumb__item  a[href="/user-groups"]' ).click()
-        cy.get( '#username').type( Cypress.env( 'USERNAME' ) );
-        cy.get( '#password' ).type( Cypress.env( 'PASSWORD' ) );
-        cy.get( '#submit' ).click();
-        cy.xpath( "//tbody/tr[2]/td[1]/a" ).click( { force: true } )
-        cy.contains( 'Edit Group' ).click( { force: true } )
-        cy.contains( 'Update Group' ).click( { force: true } )
-
-
-
-    })
-
    //Test for my profile
     it( 'Test for my profile name', () => {
         cy.contains( 'My Profile' ).click();
@@ -125,5 +167,10 @@ context( 'Test for UserGroup', () => {
 
     it( 'Test for my profile Authorization Section', () => {
         cy.get( '.pf-c-accordion' ).should( 'have.text', 'Authorization & API PermissionsUnder Development' );
+    } )
+    it( 'Test for Documentation link redirection', () => {
+        cy.get( '.pf-c-button.pf-m-link' ).should( 'contain.text', 'Documentation' ).should( 'have.attr', 'href', '/get-started/docs/apps/internal/user-groups' ).click( { force: true } );
+        cy.login( Cypress.env( 'USERNAME' ), Cypress.env( 'PASSWORD' ) );
+        cy.contains('User Groups').should('be.visible')
     })
 })
